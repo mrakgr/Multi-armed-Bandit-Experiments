2550bed1689b701d02757ed32ec8a75942ca2d1b
Marko Grdinić <mrakgr@gmail.com>
2016-11-21 16:05:20 +0100

    A non-update.
    In the last two weeks I've mostly studied Coq with a bit of Agda in the last few days for the sake of dependently typed programming.
    
    I've been enamored with the idea of moving things to the type level and making more use of the compiler features already built in, but ultimately dependent types depend on formal proofs of correctness, and those are really, really hard.
    
    https://groups.google.com/forum/#!topic/idris-lang/0JgyGwD1D5I
    
    Looking at the above posts by Jacob, they indeed make much more sense to me now, so I suppose I did learn something, but now that I know a little bit about theorem proving, I understand that I am not going to be able to use them for anything non-trivial.
    
    I actually decided to stop doing the exercises in the Software Foundations book because I got stuck enough times to trigger my stop, but those programs that I had difficulty proving were actually very simple. I can only imagine how hard would proving a normal program be.
    
    I'll probably study Idris and Agda and Coq in the future, but for now I feel a bit burnt out by it.
    
    From here I'll get back into F#.
    
    I am going to try making another version of the compiler using purely functions and regardless of whether that succeeds, I'll try to finish layer normalization and not wander off somewhere.
    
    ...I wish F# had typeclasses now, as it would allow me to further develop my style in the functional direction. Effect handlers would be nice as well. I think this last feature is a huge innovation and hopefully it will work out in Ocaml.

--- Layer Norm Stuff/Script1.fsx
+++ Layer Norm Stuff/Script1.fsx
@@ -1,9 +1,15 @@
-﻿#load "CudaCodegen.fsx"
-open SpiralV4.Flame
+﻿let str = "Hello World!"
 
-let a =
-    <@
-    let main(a: CudaGlobalArray<float32>, o: CudaGlobalArray<float32>) =
-        a.[0] <- o.[0]
-    ()
-    @>
+// Convert to an int array and group by key.
+let count_array = 
+    str.ToCharArray() 
+    |> Array.map int 
+    |> Array.groupBy id // Groups them by ints
+    // Since the ints are the same in this case, we convert the array length to counts.
+    // We also convert k back to char.
+    |> Array.map (fun (k,ar) -> char k, ar.Length) 
+    
+printfn "%A" count_array
+
+// [|('H', 1); ('e', 1); ('l', 3); ('o', 2); (' ', 1); ('W', 1); ('r', 1); ('d', 1);
+//  ('!', 1)|]
