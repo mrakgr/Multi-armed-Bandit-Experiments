45f7faff8a1ab292f9da0c84119e51fef80ef2c3
Marko Grdinić <mrakgr@gmail.com>
2016-11-24 12:11:27 +0100

    Thanks to a prompting by vitalygolub on SO, I decided to make an alternative version of the nvcc invoker that is better than what I had.
    It has proper error detection now and is a lot less hacky that the previous solution. I really did make a mistake when I mentally weighted those two options and decided to go with the previous one first.
    
    Ok, as far as calling nvcc is concerned, my needs are fully met now.
    
    I'll just add the function to the main Spiral file and then, finish that fused column reduce kernel.
    
    It is time I ended this thing and resume work on reinforcement learning. The clarion call of going to the next level with stronger static types simply too strong for me to resist.
    
    I think now, I would be satisfied if F# gets typeclasses...and higher kinded types...and effect handlers. Yeah, those would be good enough for me.

--- Layer Norm Stuff/Tests.fs
+++ Layer Norm Stuff/Tests.fs
@@ -136,26 +136,31 @@ open System
 open System.Diagnostics
 open System.IO
 
-let command_shell =
-    let procStartInfo = 
-        ProcessStartInfo(
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            RedirectStandardInput = true,
-            UseShellExecute = false,
-            FileName = "cmd")
+let compile_kernel_using_nvcc_command_prompt kernel_code kernel_name =
+    // command_shell is supposed to be outside the function, but I do not want to start a command prompt
+    // process every time I go through this file, so I've put it here for the time being.
+    
+    // At any rate, I've decided to use a variant of this function that runs a bat file instead of
+    // rerouting the command prompt.
+    use command_shell = 
+        let procStartInfo = 
+            ProcessStartInfo(
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                FileName = "cmd")
 
-    let outputHandler f (_sender:obj) (args:DataReceivedEventArgs) = f args.Data
-    let p = new Process(StartInfo = procStartInfo)
-    let print_to_standard_output = outputHandler <| fun x -> printfn "%s" x
-    p.OutputDataReceived.AddHandler(DataReceivedEventHandler (print_to_standard_output))
-    p.ErrorDataReceived.AddHandler(DataReceivedEventHandler (print_to_standard_output))
-    p.Start()
-    p.BeginOutputReadLine()
-    p.BeginErrorReadLine()
-    p
+        let outputHandler f (_sender:obj) (args:DataReceivedEventArgs) = f args.Data
+        let p = new Process(StartInfo = procStartInfo)
+        let print_to_standard_output = outputHandler <| fun x -> printfn "%s" x
+        p.OutputDataReceived.AddHandler(DataReceivedEventHandler (print_to_standard_output))
+        p.ErrorDataReceived.AddHandler(DataReceivedEventHandler (print_to_standard_output))
+        p.Start()
+        p.BeginOutputReadLine()
+        p.BeginErrorReadLine()
+        p
 
-let compile_kernel_using_shell kernel_code kernel_name =
     let quote x = sprintf "\"%s\"" x
     let quoted_vs_path_to_vcvars = Path.Combine(visual_studio_path, @"VC\bin\x86_amd64\vcvarsx86_amd64.bat") |> quote
     let quoted_vs_path_to_cl = Path.Combine(visual_studio_path, @"VC\bin\x86_amd64") |> quote
@@ -191,7 +196,55 @@ let compile_kernel_using_shell kernel_code kernel_name =
 
     command_shell.WaitForExit()
     File.ReadAllText(target_path)
+
+let compile_kernel_using_nvcc_bat_router kernel_code kernel_name =
+    let nvcc_router_path = Path.Combine(kernels_dir,"nvcc_router.bat")
+    use p = 
+        let procStartInfo = 
+            ProcessStartInfo(
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                FileName = nvcc_router_path)
+
+        let outputHandler f (_sender:obj) (args:DataReceivedEventArgs) = f args.Data
+        let p = new Process(StartInfo = procStartInfo)
+        let print_to_standard_output = outputHandler <| fun x -> printfn "%s" x
+        //p.OutputDataReceived.AddHandler(DataReceivedEventHandler (print_to_standard_output))
+        p.ErrorDataReceived.AddHandler(DataReceivedEventHandler (print_to_standard_output))
+        p
+
+    let quote x = sprintf "\"%s\"" x
+    let call x = sprintf "call %s" x
+    let quoted_vs_path_to_vcvars = Path.Combine(visual_studio_path, @"VC\bin\x86_amd64\vcvarsx86_amd64.bat") |> quote
+    let quoted_vs_path_to_cl = Path.Combine(visual_studio_path, @"VC\bin\x86_amd64") |> quote
+    let quoted_cuda_toolkit_path_to_include = Path.Combine(cuda_toolkit_path,"include") |> quote
+    let quoted_kernels_dir = kernels_dir |> quote
+    let target_path = Path.Combine(kernels_dir,kernel_name+".ptx")
+    let quoted_target_path = target_path |> quote
+    let input_path = Path.Combine(kernels_dir,"_temp.cu")
+    let quoted_input_path = input_path |> quote
     
+    File.WriteAllText(input_path,kernel_code)
+
+    let _ = 
+        use nvcc_router_file = File.OpenWrite(nvcc_router_path)
+        use nvcc_router_stream = new StreamWriter(nvcc_router_file)
+
+        nvcc_router_stream.WriteLine(call quoted_vs_path_to_vcvars)
+        nvcc_router_stream.WriteLine(
+            sprintf 
+                """nvcc -gencode=arch=compute_30,code=\"sm_30,compute_30\" --use-local-env --cl-version 2015 -ccbin %s  -I%s --keep-dir %s -maxrregcount=0  --machine 64 -ptx -cudart static  -o %s %s"""
+                quoted_vs_path_to_cl quoted_cuda_toolkit_path_to_include quoted_kernels_dir quoted_target_path quoted_input_path)
+
+    if p.Start() = false then failwith "NVCC failed to run."
+    p.BeginOutputReadLine()
+    p.BeginErrorReadLine()
+    p.WaitForExit()
+
+    if p.ExitCode <> 0 then failwithf "NVCC failed compilation with code %i" p.ExitCode
+
+    File.ReadAllText(target_path)
 
 let code = 
     """
@@ -208,6 +261,6 @@ let code =
     }
     """
 
-let k = compile_kernel_using_shell code "CPP11Test"
+let k = compile_kernel_using_nvcc_bat_router code "CPP11Test"
 
 
