1546335fc30b6a6a029ecc371d78b75538f659d2
Marko Grdinić <mrakgr@gmail.com>
2016-11-30 18:19:11 +0100

    A non-update. I had enough of Lisp. The On Lisp book is interesting, and studying it properly rather than just skimming it would be quite an undertaking, but I do not think I'll be using Lisp.
    But the essential lesson of Lisp is something that was lost on me before. If I want code that is both highly efficient and generic, the best thing would be to generate it in a DSL.
    
    This is what Spiral should be.
    
    Also I thought of generating F# code in Lisp, but I should really be generating F# code in F#.
    
    I'll let this urge that is driving me forward consume me and maybe at the end, I will get something.
    
    If I want to make my own language, the best bet would be to evolve it from here. That Cuda module was an omen of things to come. Rather than make a library, I need to evolve a set of primitives with which to interface with the world at large.

--- Spiral V5/Scripts/SpiralV5DM.fsx
+++ Spiral V5/Scripts/SpiralV5DM.fsx
@@ -16,4 +16,20 @@ open System.IO
 open System.Collections.Generic
 open System.Runtime.InteropServices
 
+type SpiralInitializer2D =
+| Init2DV1 of float32[,]
+| Init2DV2 of float32[][]
 
+type SpiralInitializer3D =
+| Init2DV1 of float32[,,]
+| Init2DV2 of float32[][][]
+
+type SpiralInitializer =
+| InitNo of dims: int list
+| InitRandom of dims: int list
+| InitHost1D of float32[]
+| InitHost2D of SpiralInitializer2D
+| InitHost3D of SpiralInitializer3D
+
+type SpiralExpr =
+| BaseNode of id: int * SpiralInitializer
