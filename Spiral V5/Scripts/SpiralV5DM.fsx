40cc616c6af3a1754e1041a6346a77e6e084633d
Marko Grdinić <mrakgr@gmail.com>
2016-12-08 18:25:56 +0100

    Did some work, but was distracted today and did not do much. And what I did does not feel good to me at all.
    It might seem strange, but I am having difficulty figuring out how the basic DM type should be structured. I'll try a different approach tomorrow.
    
    It might be a good idea to defer implementation and start from the manager instead.

--- Spiral V5/Scripts/SpiralV5DM.fsx
+++ Spiral V5/Scripts/SpiralV5DM.fsx
@@ -43,10 +43,48 @@ type CudaDeviceVariable<'t when 't: struct and 't: (new: unit -> 't) and 't:> Sy
     member inline this.Gather() =
         to_host this
 
-/// The wrapper type for the d2M and d4M. It is really unbelievable how much it simplified the code for the combinators.
-type dM =
+type DMType =
+    | DMFloat
+    | DMByte
+
+    member t.Size =
+        match t with
+        | DMFloat -> 4
+        | DMByte -> 1
+
+/// To be honest this does not feel good at all to me.
+/// I'll deal with it tomorrow. Today I was busy with other things.
+type DM =
     {
-    size : int[]
+    mutable size : int[]
+    mutable type_ : DMType // The type is tracked dynamically.
+    mutable vars : ResizeArray<CudaDeviceVariable<byte>>
     }
+
+    static member create(size: int[], type_: DMType) =
+        {size=size; type_=type_; vars=ResizeArray(2)}
+
+    member t.ResizeIf(size: int[], type_: DMType) =
+        let total_size = type_.Size * Array.reduce (*) size
+        t.size <- size
+        t.type_ <- type_
+        for i=0 to t.vars.Count do
+            if total_size > int t.vars.[i].SizeInBytes then 
+                dispose t.vars.[i]
+                t.vars.[i] <- new CudaDeviceVariable<_>(SizeT total_size)
+
+    member t.Get(i: int) =
+        if i < t.vars.Count then
+            t.vars.[i].DevicePointer
+        else
+            while t.vars.Count <= i do
+                t.vars.Add(CudaDeviceVariable<_>.Null)
+
+            t.ResizeIf(t.size,t.type_)
+            t.vars.[i].DevicePointer
+
+    interface IDisposable with
+        member t.Dispose() = for var in t.vars do var.Dispose()
+
 
 // TODO: Continue this when done testing Racket.
