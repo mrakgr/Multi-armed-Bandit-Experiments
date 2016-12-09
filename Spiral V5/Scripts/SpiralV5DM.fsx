e32a78b9b6e27555c721acfa640bee6a5ccc393c
Marko Grdinić <mrakgr@gmail.com>
2016-12-09 13:20:44 +0100

    ...It is not that this piece of code was difficult to write. It is figuring out what I wanted to write in the first place - the structure of the code that was difficult.
    Done with the first part of the new object pool along with the new, more generic DM type.
    
    Once can get a lot of genericity by simply using branches inside the getDM function.
    
    The next would be to deal with the workspace. Unlike before, I think I will put it inside the object pool this time.

--- Spiral V5/Scripts/SpiralV5DM.fsx
+++ Spiral V5/Scripts/SpiralV5DM.fsx
@@ -43,48 +43,57 @@ type CudaDeviceVariable<'t when 't: struct and 't: (new: unit -> 't) and 't:> Sy
     member inline this.Gather() =
         to_host this
 
-type DMType =
-    | DMFloat
-    | DMByte
+type VarF32 = CudaDeviceVariable<float32>
+type VarByte = CudaDeviceVariable<byte>
 
-    member t.Size =
-        match t with
-        | DMFloat -> 4
-        | DMByte -> 1
-
-/// To be honest this does not feel good at all to me.
-/// I'll deal with it tomorrow. Today I was busy with other things.
-type DM =
-    {
-    mutable size : int[]
-    mutable type_ : DMType // The type is tracked dynamically.
-    mutable vars : ResizeArray<CudaDeviceVariable<byte>>
-    }
-
-    static member create(size: int[], type_: DMType) =
-        {size=size; type_=type_; vars=ResizeArray(2)}
-
-    member t.ResizeIf(size: int[], type_: DMType) =
-        let total_size = type_.Size * Array.reduce (*) size
-        t.size <- size
-        t.type_ <- type_
-        for i=0 to t.vars.Count do
-            if total_size > int t.vars.[i].SizeInBytes then 
-                dispose t.vars.[i]
-                t.vars.[i] <- new CudaDeviceVariable<_>(SizeT total_size)
-
-    member t.Get(i: int) =
-        if i < t.vars.Count then
-            t.vars.[i].DevicePointer
-        else
-            while t.vars.Count <= i do
-                t.vars.Add(CudaDeviceVariable<_>.Null)
-
-            t.ResizeIf(t.size,t.type_)
-            t.vars.[i].DevicePointer
+type DM(size: int[], data: VarF32[]) =
+    member val Size = size with get, set
+    member val Data = data with get, set
 
     interface IDisposable with
-        member t.Dispose() = for var in t.vars do var.Dispose()
+        member t.Dispose() = for var in t.Data do var.Dispose()
 
+let float32_total_size_of size = Array.reduce (*) size
 
-// TODO: Continue this when done testing Racket.
+/// Zeroes out the fields on the first allocation.
+let new_var_float32 (total_size: int) =
+    let x = new CudaDeviceVariable<float32>(SizeT total_size)
+    x.Memset(0u)
+    x
+    
+let makeDM(size: int[], num_vars: int) =
+    let total_size = float32_total_size_of size
+    new DM(size, Array.init num_vars <| fun _ -> new_var_float32 total_size)
+
+let primal (x: DM) = let i=0 in if i < x.Data.Length then x.Data.[i] else failwith "DM does not have a primal."
+let adjoint (x: DM) = let i=1 in if i < x.Data.Length then x.Data.[i] else failwith "DM does not have an adjoint."
+let aux1 (x: DM) = let i=2 in if i < x.Data.Length then x.Data.[i] else failwith "DM does not have an aux1."
+let aux2 (x: DM) = let i=3 in if i < x.Data.Length then x.Data.[i] else failwith "DM does not have an aux2."
+
+type SpiralObjectPool() =
+    let dMPool = ResizeArray()
+    let mutable dMp = 0
+
+    member t.GetDM(size: int[], num_vars: int) =
+        let resizeIf (x: DM) = 
+            let total_size = float32_total_size_of size
+            let total_size' = float32_total_size_of x.Size
+            if x.Data.Length <> num_vars then
+                x.Data <- Array.init num_vars <| fun i ->
+                    if i < x.Data.Length then
+                        if total_size > total_size' then dispose x.Data.[i]; new_var_float32 total_size
+                        else x.Data.[i]
+                    else new_var_float32 total_size
+            elif total_size > total_size' then
+                for i=0 to x.Data.Length do
+                    dispose x.Data.[i]; x.Data.[i] <- new_var_float32 total_size
+
+        if dMPool.Count > dMp then
+            let t = dMPool.[dMp]
+            dMp <- dMp+1
+            t
+        else
+            let t = makeDM(size,num_vars)
+            dMPool.Add(t)
+            dMp <- dMp+1
+            t
