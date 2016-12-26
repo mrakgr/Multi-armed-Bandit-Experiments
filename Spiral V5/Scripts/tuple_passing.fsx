103a1a43db7d699513009758f4b328380506d2d3
Marko Grdinić <mrakgr@gmail.com>
2016-12-26 08:53:32 +0100

    "// Another try. This time with a selector function. As expected, I get a type error.
    // I pretty much wasted the entire day yesterday, but as I am so succeptible to type obsession, finally coming to grips with
    // the limit of static languages might be worth it. Inlined functions, interfaces, discriminated unions, I've been dreaming up ways of
    // using them, but I am an idiot. If I had bothered to start here and focused on just functions, I would have realized that if
    // standard F# functions cannot do it, then there no way those other contraptions can do it either.
    
    // This is the limit of the language and by extension my limit as well.
    
    // I can either accept it and realize that I do not really need more abstraction than provided by F#'s type system, or I can
    // turn to dynamic languages.
    
    // This is it. This is the hard line in the ground that I cannot cross.
    
    // And the proof is right here in this file."
    
    Check out `tuple_passing.fsx`. Really, it was always so simple. Languages are not some magic thing, Turing complete or not, F# has a well defined limit that I can either accept or not.
    
    I guess for the past six month, my mistake was wandering around too much and obsessively deliberating about types. Instead, I need to just spread my wings and let them guide me.
    
    As a new year's resolution, I won't let me focus waver with irrelevant things again. By now I should know what they are.
    
    Also, that carpal tunnel thing I mentioned yesterday is probably just smallish eczema. Meh. It is not a big deal. My hand turning stiff and itching just freaked me out for a bit, so I decided to spend a day off to let it rest.
    
    Work on the library will resume promptly.
    
    I want to get this done at all cost.
    
    It was just important that I go through this thing. If I understand the real difference between what is possible and what is not in a static language like F# then I do not have to pine for exotic things like typeclasses, GADTs or effect handlers.
    
    Now, let me start.
    
    For starters, I'll get rid of those inline functions on interpreters as they just wreck the type inference and give me no real polymorphism over standard functions with continuation passing.
    
    I'll redo the routed_add function to accept those forward and backwards continuations via a tuple. That will allow me to specify the dimension of routed via single optional argument.
    
    This will make routed_add more similar to the `cost_function` function which also takes in the dimensional extractor via an optional argument.
    
    This is the correct way to do it in a static functional language. Right now I have it done via polymorphism, but this is the optimal way given the language's various constraints.
    
    Also, given how much I've agonized over the design, Simon Peyton Jones' description of the area of corrent, but rejected programs as the `zone of undending agony` is pretty much apt.

---
+++ Spiral V5/Scripts/tuple_passing.fsx
@@ -0,0 +1,34 @@
+﻿let vale (x,_,_) = x
+let adde (_,x,_) = x
+let multe (_,_,x) = x
+
+let val_ x d =
+    let f = vale d
+    f x
+
+let add x y d =
+    let f = adde d
+    f (x d) (y d)
+
+let mult x y d =
+    let f = multe d
+    f (x d) (y d)
+
+let in_1 =
+    let val_ (x: int) = x
+    let add x y = x+y
+    let mult x y = x*y
+    val_,add,mult
+
+let in_2 =
+    let val_ (x: int) = string x
+    let add x y = sprintf "(%s + %s)" x y
+    let mult x y = sprintf "(%s * %s)" x y
+    val_,add,mult
+
+let r2 d = add (val_ 1) (val_ 3) d
+
+let test x = x in_1, x in_2 // Type error.
+
+let a2 = r2 in_1
+let b2 = r2 in_2
---
+++ Spiral V5/Scripts/tuple_passing_v1.fsx
@@ -0,0 +1,52 @@
+﻿// Another try. This time with a selector function. As expected, I get a type error.
+
+// I pretty much wasted the entire day yesterday, but as I am so succeptible to type obsession, finally coming to grips with
+// the limit of static languages might be worth it. Inlined functions, interfaces, discriminated unions, I've been dreaming up ways of
+// using them, but I am an idiot. If I had bothered to start here and focused on just functions, I would have realized that if 
+// standard F# functions cannot do it, then there no way those other contraptions can do it either.
+
+// This is the limit of the language and by extension my limit as well.
+
+// I can either accept it and realize that I do not really need more abstraction than provided by F#'s type system, or I can
+// turn to dynamic languages.
+
+// This is it. This is the hard line in the ground that I cannot cross.
+
+// And the proof is right here in this file.
+
+let vale (x,_,_) = x
+let adde (_,x,_) = x
+let multe (_,_,x) = x
+
+let val_ x d s =
+    let f = vale (s d)
+    f x
+
+let add x y d s =
+    let f = adde (s d)
+    f (x d s) (y d s)
+
+let mult x y d s =
+    let f = multe (s d)
+    f (x d s) (y d s)
+
+let in_1 =
+    let val_ (x: int) = x
+    let add x y = x+y
+    let mult x y = x*y
+    val_,add,mult
+
+let in_2 =
+    let val_ (x: int) = string x
+    let add x y = sprintf "(%s + %s)" x y
+    let mult x y = sprintf "(%s * %s)" x y
+    val_,add,mult
+
+let r2 d = add (val_ 1) (val_ 3) d
+
+//let test x = x (in_1, in_2) fst, x (in_1, in_2) snd // Type error.
+
+let a2 = r2 in_1 id
+let b2 = r2 in_2 id
+
+test r2
