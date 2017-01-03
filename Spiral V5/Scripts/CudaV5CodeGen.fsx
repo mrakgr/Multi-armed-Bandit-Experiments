e08be7c2a9af8986b4d3f541c4b91233e174c49f
Marko Grdinić <mrakgr@gmail.com>
2017-01-03 12:57:12 +0100

    I change my mind.
    I was completely right to bash monads. The style used in cuda_map_module_template is absolutely unnacceptable.
    
    ...But what are my options here? Maybe I should have gone with Lisp...
    
    ...Agh, I need to take this out. It is a failure. I'll just go with the idea of delaying computations inside function calls, that way things won't get printed out of order.
    
    I will absolutely deal with this today and move on to more important stuff...though this is quite important as well.

--- Spiral V5/Scripts/CudaV5CodeGen.fsx
+++ Spiral V5/Scripts/CudaV5CodeGen.fsx
@@ -14,20 +14,27 @@ open ManagedCuda.NVRTC
 open ManagedCuda.CudaDNN
 
 open System
+open System.Text
 open System.Diagnostics
 open System.IO
 open System.Collections.Generic
 open System.Runtime.InteropServices
 
+type ReaderBuilder() =
+    member inline t.Return a = fun r -> a
+    member inline t.Bind(a,f) = fun r -> f (a r) r
+    member inline t.ReturnFrom x = x
+    member inline t.Zero() = t.Return ()
 
+let reader = ReaderBuilder()
 
 let cuda_map_all_template 
         times plus less_than // basic operators
         gridDim_x blockDim_x blockIdx_x threadIdx_x // kernel constants
-        var for_ // kernel primitives
+        for_ // kernel primitives
         n map_macro = // kernel params
     for_ 
-        (var (plus (times blockIdx_x blockDim_x) threadIdx_x)) 
+        (plus (times blockIdx_x blockDim_x) threadIdx_x) 
         (fun i -> less_than i n) 
         (fun i -> plus i (times gridDim_x blockDim_x))
         map_macro
@@ -38,61 +45,57 @@ let cuda_tag =
         i <- i+1
         i
 
-let ind i = String.replicate i " "
+let p (x: string) (c': StringBuilder,s : int as c) = c'.Append x |> ignore
+let ind (c': StringBuilder,i : int as c) = p (String.replicate i " ") c
+let step_up (c': StringBuilder,i : int as c) = c', i+4
 
-let cuda_map_all n map_macro s =
-    let program = Text.StringBuilder()
-    let p (x: string) = program.Append x |> ignore
-
-    let plus x y = [|"(";x;" + ";y;")"|] |> String.concat ""
-    let times x y = [|"(";x;" * ";y;")"|] |> String.concat ""
-    let less_than x y = [|"(";x;" < ";y;")"|] |> String.concat ""
-    let gridDim_x, blockDim_x = "gridDim.x","blockDim.x"
-    let blockIdx_x, threadIdx_x = "blockIdx.x","threadIdx.x"
-    let var x = 
-        let v = "auto var_" + string (cuda_tag())
-        let decl = [|ind s;v;" = ";x|] |> String.concat ""
-        v
-    let for_ (v,decl) cond incr body =
-        [|ind s;"for(";decl;", ";cond v;", ";incr v;") {\n"|] |> Array.iter p
-        body v (s+4) |> p
-        [|ind s;"}\n"|] |> Array.iter p
+let cuda_map_all n map_macro =
+    let plus x y c = p "(" c;x c;p " + " c;y c;p ")" c
+    let times x y c = p "(" c;x c;p " + " c;y c;p ")" c
+    let less_than x y c = p "(" c;x c;p " + " c;y c;p ")" c
+    let gridDim_x c = p "gridDim.x" c
+    let blockDim_x c = p "blockDim.x" c
+    let blockIdx_x c = p "blockIdx.x" c
+    let threadIdx_x c = p "threadIdx.x" c
+    let for_ init cond incr body c =
+        let v c = p ("auto var_" + string (cuda_tag())) c
+        ind c;p "for(" c;v c;p " = " c;init c;p "; " c;cond v c;p "; " c;incr v c;p ") {\n" c
+        body v (step_up c)
+        ind c;p "}\n" c
         
     cuda_map_all_template 
         times plus less_than // basic operators
         gridDim_x blockDim_x blockIdx_x threadIdx_x // kernel constants
-        var for_ // kernel primitives
+        for_ // kernel primitives
         n map_macro
 
 let cuda_map_module_template 
         n args kernel_name map_macro // kernel params
-        method_ externCBlock include_ = // kernel primitives
-        
-    include_ "thrust/tuple.h"
-    include_ "cub/cub.cuh"
-    externCBlock (
-        method_ "__global__ void " kernel_name args (
-            cuda_map_all n map_macro
-            )
-        )
+        method_ externCBlock include_ c = // kernel primitives
+    reader {
+        let! _ = include_ "thrust/tuple.h"
+        let! _ = include_ "cub/cub.cuh"
+        return
+            externCBlock (
+                method_ "__global__ void " kernel_name args (
+                    cuda_map_all n map_macro
+                    )
+                )
+        }
 
 let cuda_map_module_compile map_module =
-    let program = Text.StringBuilder()
-    let p (x: string) = program.Append x |> ignore
-
-    let method_ rtyp kernel_name args body s =
-        [|ind s;rtyp;kernel_name;"(";args;") {\n"|] |> Array.iter p
-        body (s+4) |> p
-        [|ind s;"}"|] |> Array.iter p
-    let externCBlock body =
-        [|"extern \"C\" {"|] |> Array.iter p
-        body 4
-        [|"}\n"|] |> Array.iter p
-    let include_ str =
-        p "#include "; p (quote str); p "\n"
+    let method_ rtyp kernel_name args body c =
+        ind c;p rtyp c;p kernel_name c;p "(" c;p args c;p ") {\n" c
+        body (step_up c)
+        ind c;p "}" c
+    let externCBlock body c =
+        p "extern \"C\" {" c
+        body (step_up c)
+        p "}\n" c
+    let include_ str c =
+        p "#include " c; p (quote str) c; p "\n" c
 
     map_module method_ externCBlock include_
-    program.ToString()
 
 let cudavar_template get_method_arg get_var name typ size =
     let method_arg = get_method_arg name typ
@@ -131,30 +134,30 @@ let map_module_template
        
     cuda_map_module_compile f
     
-let cuda_group (num_in, names_in) =
-    let f i n = n + string i
-    List.collect (fun (i: int) ->
-        List.map (f i) names_in) [1..num_in]
-
-let mapcoef_module_forward args_in args_coef args_out kernel_name map_macro =
-    let size_arg, size_var = cudavar_var "n" "const int "
-    let process_ins (ins,consts) _ =
-        let ins_arg, ins_var = 
-            List.map (fun n -> cudavar_ar1d n "const float *" size_arg) (cuda_group ins)
-            |> List.unzip
-        let consts_arg, consts_var =
-            List.map (fun n -> cudavar_var n "const float ") (cuda_group consts)
-            |> List.unzip
-        size_arg,(ins_arg,consts_arg),(size_var,ins_var,consts_var)
-
-    let process_outs _ outs =
-        let outs_arg, outs_var = 
-            List.map (fun n -> cudavar_ar1d n "float *" size_arg) (cuda_group outs)
-            |> List.unzip
-        outs_arg,outs_var
-
-    let process_args size_arg (ins_arg, consts_arg) outs_arg =
-        size_arg :: ([ins_arg;consts_arg;outs_arg] |> List.concat) |> String.concat ", "
-    
-    map_module_template process_ins process_outs process_args (args_in, args_coef) args_out kernel_name map_macro
-
+//let cuda_group (num_in, names_in) =
+//    let f i n = n + string i
+//    List.collect (fun (i: int) ->
+//        List.map (f i) names_in) [1..num_in]
+//
+//let mapcoef_module_forward args_in args_coef args_out kernel_name map_macro =
+//    let size_arg, size_var = cudavar_var "n" "const int "
+//    let process_ins (ins,consts) _ =
+//        let ins_arg, ins_var = 
+//            List.map (fun n -> cudavar_ar1d n "const float *" size_arg) (cuda_group ins)
+//            |> List.unzip
+//        let consts_arg, consts_var =
+//            List.map (fun n -> cudavar_var n "const float ") (cuda_group consts)
+//            |> List.unzip
+//        size_arg,(ins_arg,consts_arg),(size_var,ins_var,consts_var)
+//
+//    let process_outs _ outs =
+//        let outs_arg, outs_var = 
+//            List.map (fun n -> cudavar_ar1d n "float *" size_arg) (cuda_group outs)
+//            |> List.unzip
+//        outs_arg,outs_var
+//
+//    let process_args size_arg (ins_arg, consts_arg) outs_arg =
+//        size_arg :: ([ins_arg;consts_arg;outs_arg] |> List.concat) |> String.concat ", "
+//    
+//    map_module_template process_ins process_outs process_args (args_in, args_coef) args_out kernel_name map_macro
+//
