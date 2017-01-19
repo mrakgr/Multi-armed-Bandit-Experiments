878a1266f090018c0ba095beac727c4e367f6973
Marko Grdinić <mrakgr@gmail.com>
2017-01-19 19:22:06 +0100

    Despite not doing much, just making these expressions made me dead tired.
    I think I will do some Haskell reading.
    
    Actually I will just call it a day here.
    
    Tomorrow I might either do work on statements for the Cuda compiler, or I need to figure out some experiments on understanding stacked monads. I am still not entirely clear on how they are supposed to work.
    
    But I am not sure what sort of experiment to try.
    
    At any rate, instead of doing reading, as I am so mentally exhausted as to nearly drop dead, I'll call it a day here. This has been happening a lot to me lately after programming in Haskell.
    
    But I am also getting the sense that I am internalizing a lot.

--- Spiral-V5-Haskell/modules_v2.hs
+++ Spiral-V5-Haskell/modules_v2.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString (ByteString)
 
 infixl 1 |>
 (|>) = flip ($)
@@ -23,30 +25,29 @@ data CudaExpr x where
   GT' :: (Show x, Ord x) => CudaExpr x -> CudaExpr x -> CudaExpr Bool
   GTE' :: (Show x, Ord x) => CudaExpr x -> CudaExpr x -> CudaExpr Bool
 
+  Exp :: (Show x, Num x) => CudaExpr x -> CudaExpr x
+  Log :: (Show x, Num x) => CudaExpr x -> CudaExpr x
+  Tanh :: (Show x, Num x) => CudaExpr x -> CudaExpr x
+  Neg :: (Show x, Num x) => CudaExpr x -> CudaExpr x
+
+  Or :: CudaExpr Bool -> CudaExpr Bool -> CudaExpr Bool
+  And :: CudaExpr Bool -> CudaExpr Bool -> CudaExpr Bool
+
   If :: Show x => CudaExpr Bool -> CudaExpr x -> CudaExpr x -> CudaExpr x
 
-eval :: CudaExpr x -> x
-eval (C x) = x
-eval (Add a b) = eval a + eval b
-eval (Sub a b) = eval a - eval b
-eval (Mul a b) = eval a * eval b
-eval (Div a b) = eval a / eval b
+parenths x = ["(", x, ")"] |> pw'
+pw' = B.intercalate " "
+pw = parenths . pw'
 
-eval (EQ' a b) = eval a == eval b
-eval (LT' a b) = eval a < eval b
-eval (LTE' a b) = eval a <= eval b
-eval (GT' a b) = eval a > eval b
-eval (GTE' a b) = eval a >= eval b
-eval (If cond true false) = if eval cond then eval true else eval false
+fun_call f x = [f, "(", args, ")"] |> pw' where
+  args' = map pr x
+  args = B.intercalate ", " args'
+unop x a = [x, pr a] |> pw
+binop a x b = [pr a, x, pr b] |> pw
+triop x a y b z c = [x, pr a, y, pr b, z, pr c] |> pw
 
-p = id
-pw x = ["(", p x, ")"] |> concat
-
-binop a x b = [pr a, p x, pr b] |> concat |> pw
-triop x a y b z c = [p x, pr a, p y, pr b, p z, pr c] |> concat |> pw
-
-pr :: Show x => CudaExpr x -> String
-pr (C x) = show x
+pr :: Show x => CudaExpr x -> ByteString
+pr (C x) = B.pack $ show x
 pr (Add a b) = binop a "+" b
 pr (Mul a b) = binop a "*" b
 pr (Sub a b) = binop a "-" b
@@ -57,9 +58,18 @@ pr (LT' a b) = binop a "<" b
 pr (LTE' a b) = binop a "<=" b
 pr (GT' a b) = binop a ">" b
 pr (GTE' a b) = binop a ">=" b
+
+pr (Or a b) = binop a "||" b
+pr (And a b) = binop a "&&" b
+
+pr (Neg x) = unop "-" x
+pr (Exp x) = fun_call "exp" [x]
+pr (Log x) = fun_call "log" [x]
+pr (Tanh x) = fun_call "tanh" [x]
+
 pr (If cond true false) = triop "if" cond "then" true "else" false
 
-q = (Add (C (2 :: Float)) (C 3)) |> Mul (C 6) |> pr
+q = (Add (C (2 :: Float)) (C 3)) |> Mul (C 6) |> Log |> pr
 
 main :: IO ()
 main = print "Hello"
