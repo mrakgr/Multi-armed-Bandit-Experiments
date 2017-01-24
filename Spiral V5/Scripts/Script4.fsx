331e8e0462f7bbd73a91bd446f162a49126b27fb
Marko Grdinić <mrakgr@gmail.com>
2017-01-24 11:32:51 +0100

    Another type erorr that I can't figure out. This would be enough to trigger my `blame the language` rule.
    Yeah, statically resolved type params aren't typeclasses. They could be enough in theory, but it is safe to say that I was 100% wrong on this. The tagless style is even more difficult to emulate than what I'd imagined in F#.
    
    Yeah, the last 3 weeks were despressing as hell for me.
    
    Should I pack up my bags and move? Or should I do some kind of after action report as per Gustavo's suggestion?
    
    I've been thinking about doing a shallow DSL, but that would be the same as the second Cuda compiler.
    
    ....Yeah, I do not feel like digging through the past commits. I'll let the sleeping dogs lie. Today and yesterday I pretty much proved that F# lacks the necessary type system features to let me implement what i want.
    
    Between Haskell and Ocaml, with Haskell, I just might be able to take advantage of the Cuda bindings for it.
    
    But with Ocaml, I might be able to adapt whatever Linux bindings it has for Windows.
    
    But Haskell's laziness aside, I never want to go through the pain of having the language I am using insufficient to implement my vision.
    
    In that regard Haskell is in a league of its own.
    
    There is no choice - I think I will give it a serious try.

--- Spiral V5/Scripts/Script4.fsx
+++ Spiral V5/Scripts/Script4.fsx
@@ -47,48 +47,6 @@ type CudaAr1D<'t> = CudaAr1D of CudaScalar<int> * name: string with
         cuda_expr<'t> expr
 let inline cuda_ar1d<'t> size name = CudaAr1D(size, name) : CudaAr1D<'t>
 
-type CudaAr2D<'t> = CudaAr2D of CudaScalar<int> * CudaScalar<int> * name: string with
-    member t.Name = t |> fun (CudaAr2D (_, _, name)) -> name
-    member t.Access((x: CudaScalar<int>, y: CudaScalar<int>)) =
-        let name,size2 = t |> fun (CudaAr2D(size1,size2,name)) -> name, size2.Name
-        let scalar_x_name = x.Name
-        let scalar_y_name = y.Name
-        let expr = sprintf "%s[%s * %s + %s]" name scalar_x_name size2 scalar_y_name
-        cuda_expr<'t> expr
-
-let inline cuda_ar2d<'t> (s1,s2) name = CudaAr2D(s1,s2,name) : CudaAr2D<'t>
-
-let inline access x accessor = (^a: (member Access: ^ac -> CudaExpr< ^ex>) x, accessor)
-let inline expr x = String.concat "" x |> cuda_expr
-
-let inline unary_fun_op op (x: CudaExpr<'a>) = [|op;"(";x.Expr;")"|] |> expr : CudaExpr<'a>
-let inline unary_op op x = [|"(";op;x;")"|] |> expr
-let inline binary_op op (x: CudaExpr<'e>) (y: CudaExpr<'e>) = [|"(";x.Expr;op;y.Expr;")"|] |> expr 
-
-let inline exp x = unary_fun_op "exp" x
-let inline log x = unary_fun_op "log" x
-let inline tanh x = unary_fun_op "tanh" x
-
-type CudaExpr<'t> with
-    static member inline (~-)(x) = unary_op "-" x
-
-    static member inline (<)(x,y) = binary_op " < " x y : CudaExpr<bool>
-    static member inline (<=)(x,y) = binary_op " <= " x y : CudaExpr<bool>
-    static member inline (=)(x,y) = binary_op " == " x y : CudaExpr<bool>
-    static member inline (>)(x,y) = binary_op " > " x y : CudaExpr<bool>
-    static member inline (>=)(x,y) = binary_op " >= " x y : CudaExpr<bool>
-
-    static member inline (||)(x,y) = binary_op " || " x y : CudaExpr<bool>
-    static member inline (&&)(x,y) = binary_op " && " x y : CudaExpr<bool>
-
-    static member inline (+) ((x: CudaExpr<'a>), y) = binary_op " + " x y : CudaExpr<'a>
-    static member inline (*) ((x: CudaExpr<'a>), y) = binary_op " * " x y : CudaExpr<'a>
-    static member inline (/) ((x: CudaExpr<'a>), y) = binary_op " / " x y : CudaExpr<'a>
-    static member inline (-) ((x: CudaExpr<'a>), y) = binary_op " - " x y : CudaExpr<'a>
-
-let inline if_ (cond: CudaExpr<bool>) (true_: CudaExpr<'a>) (false_: CudaExpr<'a>) = 
-    [|"((";cond.Expr;") ? (";true_.Expr;") : (";false_.Expr;"))"|] |> expr : CudaExpr<'a>
-
 type TypePrinter<'t>() = class end
 
 let inline print_type x =
@@ -111,9 +69,6 @@ type ArgsPrinter with
     static member inline PrintArg(_: ArgsPrinter, t: CudaAr1D< ^t>) = 
         [|print_type (TypePrinter< ^t>()); "*"; t.Name|] |> String.concat " "
 type ArgsPrinter with
-    static member inline PrintArg(_: ArgsPrinter, t: CudaAr2D< ^t>) = 
-        [|print_type (TypePrinter< ^t>()); "*"; t.Name|] |> String.concat " "
-type ArgsPrinter with
     static member inline PrintArg(_: ArgsPrinter, (x1, x2)) = 
         [|print_arg x1;print_arg x2|] |> String.concat ", "
 type ArgsPrinter with
@@ -132,41 +87,7 @@ type CudaProgram =
 | Dedent
 | Statements of ResizeArray<CudaProgram>
 
-let process_statements (statements: ResizeArray<CudaProgram>) =
-    let rec process_statement (code: StringBuilder,ind as state) statement =
-        match statement with
-        | Statement x -> [|String.replicate ind " "; x; "\n"|] |> expr |> code.Append, ind
-        | Indent -> code, ind+4
-        | Dedent -> code, ind-4
-        | Statements x -> process_statements state x
-    and process_statements state (statements: ResizeArray<CudaProgram>) =
-        Seq.fold process_statement state statements
-    process_statements (StringBuilder(),0) statements
-    |> fun (code,ind) -> code.ToString()
-
 let state x (program: ResizeArray<_>) = x |> String.concat "" |> Statement |> program.Add
-let enter body v (program: ResizeArray<_>) =
-    Indent |> program.Add
-    body v
-    Dedent |> program.Add
-
-let (|?>) a b program = a program; b program; ()
-
-// Outer language.
-
-let inline method_ rtype kernel_name ins consts outs body =
-    let args = print_arg (ins, consts, outs)
-    state [|rtype;kernel_name;"(";args;") {"|]
-    |?> enter body ()
-    |?> state [|"}"|]
-
-let externCBlock body =
-    state [|"extern \"C\" {"|]
-    |?> enter body ()
-    |?> state [|"}"|]
-
-let include_ str =
-    state [|"#include "; quote str|]
 
 // Inner language.
 
@@ -182,3 +103,4 @@ let inline var_1d (size: CudaExpr<int>) (init: CudaExpr<'t>) program =
     let size: CudaScalar<int> = var_scalar size program // Type error
     let var = cuda_ar1d size var_name : CudaAr1D<'t>
     state [|typ;" ";var_name;"[";size.Name;"]";" = ";init.Expr;";"|] program; var
+
