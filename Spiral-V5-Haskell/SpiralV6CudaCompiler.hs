f33b603586fb8be3ea73367c2f737beda180686a
Marko Grdinić <mrakgr@gmail.com>
2017-01-24 17:50:43 +0100

    It seems that thanks to the RebindableSyntax extension, Haskell's syntax is more maleable than I thought. This is actually a great news as far as my Cuda DSL is concerned. Syntax was kind of a pain point in F# as well, so I am glad I found this.
    At any rate, I am done with the evaluator for the expressions part of the DSL is done. For today I had enough. It took me almost 3 weeks to defect to Haskell, and it has been quite draining for me emotionally. I will have to leave so much behind, but now that I have the skill, maybe I have what it takes to rebuild much faster than I think.
    
    Depending on the state of the Cuda package, I might have to make my own bindings for the library. This will be a pain in the ass - ManagedCuda was one of the reasons why I kept delaying the decision to make the move.
    
    But with Haskell, I will do absolutely everything right this time. Absolutely.
    
    No doubt that in terms of language features, the thing is unparalleled. I won't have to agnoze about expressing things in it.
    
    And rather than use my own DSL, I might end up piggybacking of Accelerate for the map operations.
    
    The most important is that move on from the conflict raging inside of me that I had suffer for the past three weeks because insufficient language features of F#.
    
    Peace of mind is something that money can't buy, but Haskell might give me.
    
    A programming is only good as his abstractions.
    
    Tomorrow, I will grip the reigns on my will and ride onwards.
    
    I'll break out the state monad and start work on the statements part of the DSL.

--- Spiral-V5-Haskell/SpiralV6CudaCompiler.hs
+++ Spiral-V5-Haskell/SpiralV6CudaCompiler.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RebindableSyntax #-}
 
 module Main where
 
 import qualified Prelude as P
+import Data.String (fromString)
 import qualified Data.ByteString.Char8 as B
 import Data.ByteString.Char8 (ByteString)
-import Prelude hiding ((+),(-),(*),(/),(<=),(<),(==),(>),(>=))
+import Prelude hiding ((+),(-),(*),(/),(<=),(<),(==),(>),(>=),negate,exp,log,tanh)
+
+-- ifThenElse True true false = true
+-- ifThenElse False true false = false
 
 infixl 1 |>
 (|>) = flip ($)
@@ -15,13 +20,16 @@ infixl 1 |>
 class CudaConst repr where
   c :: Show x => x -> repr x
 
+infixl 6 +,-
+infixl 7 *,/
 class CudaArith repr where
-  (~-) :: Num x => repr x -> repr x
+  negate :: Num x => repr x -> repr x
   (+) :: Num x => repr x -> repr x -> repr x
   (-) :: Num x => repr x -> repr x -> repr x
   (*) :: Num x => repr x -> repr x -> repr x
   (/) :: Fractional x => repr x -> repr x -> repr x
 
+infix 4 <=,<,==,>,>=
 class CudaComp repr where
   (<=) :: Ord x => repr x -> repr x -> repr Bool
   (<) :: Ord x => repr x -> repr x -> repr Bool
@@ -30,11 +38,18 @@ class CudaComp repr where
   (>=) :: Ord x => repr x -> repr x -> repr Bool
 
 class CudaBoolOps repr where
+  infixr 2 ||
   (||) :: repr Bool -> repr Bool -> repr Bool
+  infixr 3 &&
   (&&) :: repr Bool -> repr Bool -> repr Bool
 
 class CudaCond repr where
-  if_ :: repr Bool -> repr x -> repr x -> repr x
+  ifThenElse :: repr Bool -> repr x -> repr x -> repr x
+
+class CudaFun repr where
+  exp :: Floating x => repr x -> repr x
+  log :: Floating x => repr x -> repr x
+  tanh :: Floating x => repr x -> repr x
 
 newtype Eval x = Eval {eval :: x} deriving Show
 
@@ -44,67 +59,84 @@ instance CudaConst Eval where
 bo f a b = eval a `f` eval b |> Eval
 
 instance CudaArith Eval where
-  --(~-) = bo P.-
+  negate = Eval . P.negate . eval
   (+) = bo (P.+)
-  -- (-) = bo P.-
-  -- (*) = bo P.*
-  -- (/) = bo P./
+  (-) = bo (P.-)
+  (*) = bo (P.*)
+  (/) = bo (P./)
 
--- instance CudaComp Eval where
---   (<=) = bo P.<=
---   (<) = bo P.<
---   (==) = bo P.==
---   (>) = bo P.>
---   (>=) = bo P.>=
---
--- instance CudaBoolOps Eval where
---   (&&) = bo P.&&
---   (||) = bo P.||
---
--- instance CudaCond Eval where
---   if_ cond true false =
---     Eval $ if eval cond then eval true else eval false
---
--- newtype Print x = Print {pr :: ByteString} deriving Show
---
--- parenths x = ["(", x, ")"] |> pw'
--- pw' = B.intercalate " "
--- pw = parenths . pw'
---
--- fun_call f x = [f, "(", args, ")"] |> pw' where
---   args' = map pr x
---   args = B.intercalate ", " args'
---
--- unop x a = [x, pr a] |> pw |> Print
--- binop x a b = [pr a, x, pr b] |> pw |> Print
--- triop x y z a b c = [x, pr a, y, pr b, z, pr c] |> pw |> Print
---
--- instance CudaConst Print where
---   c = Print . B.pack . show
---
--- instance CudaArith Print where
---   (+) = binop "+"
---   (-) = binop "-"
---   (*) = binop "*"
---   (/) = binop "/"
---
--- instance CudaComp Print where
---   (<=) = binop "<="
---   (<) = binop "<"
---   (==) = binop "=="
---   (>) = binop ">"
---   (>=) = binop ">="
---
--- instance CudaBoolOps Print where
---   (&&) = binop "&&"
---   (||) = binop "||"
---
--- instance CudaCond Print where
---   if_ = triop "if" "then" "else"
---
--- test = (c 1) + (c 2) * (c 3)
---
--- q = eval test
--- w = (pr :: Print Float -> ByteString) test
---
+instance CudaComp Eval where
+  (<=) = bo (P.<=)
+  (<) = bo (P.<)
+  (==) = bo (P.==)
+  (>) = bo (P.>)
+  (>=) = bo (P.>=)
+
+instance CudaBoolOps Eval where
+  (&&) = bo (P.&&)
+  (||) = bo (P.||)
+
+instance CudaCond Eval where
+  ifThenElse cond true false =
+    case eval cond of
+      True -> Eval $ eval true
+      False -> Eval $ eval false
+
+uf f = Eval . f . eval
+
+instance CudaFun Eval where
+  exp = uf P.exp
+  log = uf P.log
+  tanh = uf P.tanh
+
+newtype Print x = Print {pr :: ByteString} deriving Show
+
+parenths x = ["(", x, ")"] |> pw'
+pw' = B.concat
+pw = parenths . pw'
+
+fun_call f x = [f, "(", args, ")"] |> pw' |> Print where
+  -- args' = map pr x
+  -- args = B.intercalate ", " args'
+  args = pr x
+
+space_out x = B.concat [" ", x, " "]
+unop x a = [x, pr a] |> pw |> Print
+binop x a b = [pr a, space_out x, pr b] |> pw |> Print
+triop x y z a b c = [x, pr a, space_out y, pr b, space_out z, pr c] |> pw |> Print
+
+instance CudaConst Print where
+  c = Print . B.pack . show
+
+instance CudaArith Print where
+  negate = unop "-"
+  (+) = binop "+"
+  (-) = binop "-"
+  (*) = binop "*"
+  (/) = binop "/"
+
+instance CudaComp Print where
+  (<=) = binop "<="
+  (<) = binop "<"
+  (==) = binop "=="
+  (>) = binop ">"
+  (>=) = binop ">="
+
+instance CudaBoolOps Print where
+  (&&) = binop "&&"
+  (||) = binop "||"
+
+instance CudaCond Print where
+  ifThenElse = triop "" "?" ":"
+
+instance CudaFun Print where
+  exp = fun_call "exp"
+  log = fun_call "log"
+  tanh = fun_call "tanh"
+
+test = if c True then c 1 + c 2 * (- (c 3)) else c 999 |> log
+
+q = eval test
+w = pr test
+
 main = print "Hello"
