dd77004e93960b11e646ed8a3075dedfcdb082f4
Marko Grdinić <mrakgr@gmail.com>
2017-01-30 20:03:32 +0100

    http://cs.brown.edu/courses/cs173/2012/book/types.html
    Today is such an embarassment for me. I was supposed to be making a DSL in Haskell and yet I do not feel like it.
    
    So at the very least, I decided to place a few blocks with the energy that I have left. With this I'll call it a day.
    
    There is no greater shame than losing one's drive to me.
    
    I grasped for the sky a bit too early, but unlike with the self improvement algorithm I lovingly named Necromutation, I can access the higher level of functional programming even today.
    
    I will do this.
    
    Even if I did the damn thing in Haskell, I would still have to implement the type inference algorithm in order to assist my neural net agents in the future.
    
    So I might as well do it now.
    
    This is the proof of my determination - and my care.

---
+++ Spiral V5/Scripts/Typechecker.fsx
@@ -0,0 +1,47 @@
+﻿// http://cs.brown.edu/courses/cs173/2012/book/types.html
+
+type Number = int
+type Symbol = string
+
+type Type = 
+| NumT
+| FunT of Type * Type
+
+type TyExprC =
+| NumC of Number
+| IdC of Symbol
+| AppC of f: TyExprC * arg: TyExprC
+| PlusC of TyExprC * TyExprC
+| MultC of TyExprC * TyExprC
+| LamC of arg: Symbol * argT: Type * retT: Type * body: TyExprC
+
+type Env = Map<Symbol, Value>
+
+and Value =
+| NumV of Number
+| ClosV of Symbol * TyExprC * Env
+
+type TyEnv = Map<Symbol, Type>
+
+let get_type_of n tenv =
+    match Map.tryFind n tenv with
+    | Some v -> v
+    | None -> failwith "Identifier not found in the type environment."
+
+let rec tc expr tenv = 
+    match expr with
+    | NumC _ -> NumT
+    | IdC n -> get_type_of n tenv
+    | PlusC (l,r) | MultC (l,r) -> 
+        match tc l tenv, tc r tenv with
+        | NumT, NumT -> NumT
+        | _ -> failwith "not both numbers"
+    | AppC (f,a) ->
+        let ft, at = tc f tenv, tc a tenv
+        match ft,at with
+        | FunT(ft',at'), at -> if at' = at then ft else failwith "app arg mismatch"
+        | _, _ -> failwith "not a function"
+    | LamC (name,argT,retT,body) ->
+        if retT = tc body (tenv.Add (name, argT)) then FunT(argT,retT)
+        else failwith "lam type mismatch"
+
