b8c779388a5ac243e88fba693931c19d24d2cae3
Marko Grdinić <mrakgr@gmail.com>
2017-02-26 14:50:28 +0100

    Ok, this is it. ith this I've implemented statically checked, higher ranked polymorphism that ML languages do not support.
    The next step would be to implement typecase for dependent inlineables.

--- Spiral V5/Try Two/Typechecker.fsx
+++ Spiral V5/Try Two/Typechecker.fsx
@@ -2,34 +2,44 @@
     | Int
     | Float
     | Bool
+    | Tuple of Ty list
 
 type Expr = 
-    | Var of string
-    | App of Expr * Expr
+    | V of string
+    | Inline of Expr * Expr list
     | If of Expr * Expr * Expr
-    | Lam of string * Expr
+    | Inlineable of string list * Expr
     | Let of string * Expr * Expr
+
     | LitInt of int
     | LitFloat of float
     | LitBool of bool
+    | T of Expr list // T stands for tuple
 
 type TyEvalResult =
     | Ty of Ty
-    | Expr of Expr // Supposed to be only Lam. Otherwise it would lead to the language having unhigienic macros.
+    | Expr of Expr
 
 type EnvType = Map<string,TyEvalResult>
 
 let rec teval (env: EnvType) exp =
     match exp with
-    | Var x -> 
+    | V x -> 
         match Map.tryFind x env with
         | Some x -> x
         | None -> failwith "Variable not bound."
-    | App(l,r) ->
-        let l,r = teval env l, teval env r
-        match l,r with
-        | Expr(Lam(x,b)), (Ty _ as t) -> teval (Map.add x t env) b
-        | _ -> failwithf "Expected: lambda, type\nGot: %A and %A instead" l r
+    | Inline(l,r) ->
+        let l,r = teval env l, List.map (teval env) r
+        match l with
+        | Expr(Inlineable(x,b)) -> 
+            let env = 
+                List.fold (fun env (arg,typ) ->
+                    Map.add arg typ env
+                    ) env (List.zip x r)
+            match teval env b with
+            | Expr _ as x -> failwithf "Expected: base type\nGot: %A" x
+            | Ty _ as x -> x
+        | _ -> failwithf "Expected: inlineable, type\nGot: %A and %A instead" l r
     | If(cond,tr,fl) ->
         match teval env cond with
         | Ty Bool -> ()
@@ -43,11 +53,34 @@ let rec teval (env: EnvType) exp =
     | LitInt _ -> Ty Int
     | LitFloat _ -> Ty Float
     | LitBool _ -> Ty Bool
-    | Lam _ as x -> Expr x
+    | T x -> 
+        let to_ty = function
+            | Expr _ as x -> failwithf "Expected: base type\nGot: %A" x
+            | Ty x -> x
+        List.map (to_ty << teval env) x |> Tuple |> Ty
+    | Inlineable _ as x -> Expr x
 
-let lam x y = Lam(x,y)
-let app x y = App(y,x)
+let inl x y = Inlineable(x,y)
+let inap x y = Inline(x,y)
+let l x b i = Let(x,b,i)
 
 // Some tests
-let term1 = lam "x" (lam "y" (Var "x")) |> app (LitInt 1) |> app (LitInt 2)
+let term1 = inap (inl ["x";"y"] (V "x")) [LitInt 1; LitInt 2] 
 let t1 = teval Map.empty term1
+
+let term2 =
+    l "inlineable" 
+        (inl ["x";"y"] (T [V "x";V "y"]))
+        (T [(inap (V "inlineable") [LitInt 1; LitInt 2]); 
+            (inap (V "inlineable") [LitFloat 1.5; LitInt 2])])
+let t2 = teval Map.empty term2
+
+let term3 =
+    l "inlineable" 
+        (inl ["x";"y"] (T [V "x";V "y"]))
+        (l "fun" 
+            (inl ["inl";"a";"b";"c";"d"] 
+                (T [inap (V "inl") [V "a";V "b"];
+                    inap (V "inl") [V "c";V "d"]]))
+            (inap (V "fun") [V "inlineable"; LitInt 1; LitInt 2; LitFloat 1.5; LitInt 2]))
+let t3 = teval Map.empty term3
