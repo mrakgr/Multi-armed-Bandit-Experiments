b344ed74bb78130ddc03477797c15df344a059f3
Marko Grdinić <mrakgr@gmail.com>
2017-02-28 13:08:35 +0100

    I got the little macro inliner to work. And the trick of saving the env in inline_args was quite ingenious.
    Now, to make my macro function hygienic, I'll just add variable tagging. After that the only thing left will be global functions and I will have completed the very first version of the language.
    
    To be honest, When I got up I wanted to play Skyrim (I started playing it again after two years,) but now I am into it.

--- Spiral V5/Cuda Backend/Typechecker_v2.fsx
+++ Spiral V5/Cuda Backend/Typechecker_v2.fsx
@@ -46,25 +46,79 @@ let rec teval (env: EnvType) inline_args exp: ReturnCases =
         | Some (RTypedExpr _ as v) -> v
         | Some (RExpr(v,env)) -> teval env inline_args v
         | Some (RError _ as e) -> e
-        | None -> RError "Variable not bound."
+        | None -> RError <| sprintf "Variable %A not bound." x
     | Inline(expr,args) ->
-        teval env (args :: inline_args) expr
+        teval env ((args,env) :: inline_args) expr
     | If(cond,tr,fl) as orig ->
         match teval env inline_args cond with
         | RTypedExpr cond' when get_type cond' = Bool ->
             match teval env inline_args tr, teval env inline_args fl with
-            | RTypedExpr tr, RTypedExpr fl -> RTypedExpr <| TyIf(cond',tr,fl,get_type tr)
-            | RExpr(tr',_), RExpr(fl',_) -> RExpr(If(cond,tr',fl'),env) // tr = tr', fl = fl'
+            | RTypedExpr tr, RTypedExpr fl -> 
+                if get_type tr = get_type fl then
+                    RTypedExpr <| TyIf(cond',tr,fl,get_type tr)
+                else
+                    RError "Types in branches of if do not match."
+            | RExpr(_,_), RExpr(_,_) -> RExpr(orig,env)
             | a, b -> RError <| sprintf "Expected either both equal types or non-evaluated in conditional.\nGot true: %A\nGot false: %A" a b
-        | x -> RError <| sprintf "Expected bool in conditionalal.\nGot: %A" x
+        | x -> RError <| sprintf "Expected bool in conditional.\nGot: %A" x
+    | Inlineable(args,body) as inl ->
+        match inline_args with
+        | (cur_args,env'') :: other_args ->
+            if args.Length = cur_args.Length then
+                let env =
+                    (args, cur_args) ||>
+                    List.fold2 (fun env' arg cur_arg -> 
+                        Map.add arg (teval env'' [] cur_arg) env'
+                        ) env
+                teval env other_args body
+            else
+                RError "args.Length = cur_args.Length failed in the Inlineable case"
+        | [] -> RExpr(inl,env)
     | Let(v,b,e) as orig ->
         match teval env [] b with
-        | RTypedExpr b' as b'' ->
+        | RTypedExpr b' ->
             let b'_type = get_type b'
             let v' = TyV(v,b'_type)
             match teval (Map.add v (RTypedExpr v') env) inline_args e with
             | RTypedExpr e' -> RTypedExpr(TyLet((v,b'_type),b',e',get_type e'))
-            | RExpr(e',_) -> RExpr(Let(v,b,e'),env) // e = e'
+            | RExpr(_,_) -> RExpr(orig,env)
             | RError er -> RError er
         | RExpr _ as b -> teval (Map.add v b env) inline_args e
         | RError _ as e -> e
+    | LitInt x -> RTypedExpr (TyLitInt x)
+    | LitFloat x -> RTypedExpr (TyLitFloat x)
+    | LitBool x -> RTypedExpr (TyLitBool x)
+    | T ls as orig ->
+        let rec loop acc = function
+            | l::ls ->
+                match teval env inline_args l with
+                | RTypedExpr x -> loop (x::acc) ls
+                | RExpr _ as x -> RExpr (orig, env)
+                | RError _ as x -> x
+            | [] -> RTypedExpr (TyT acc)
+        loop [] ls
+
+let inl x y = Inlineable(x,y)
+let inap x y = Inline(x,y)
+let l x b i = Let(x,b,i)
+
+// Some tests
+let term1 = inap (inl ["x";"y"] (V "x")) [LitInt 1; LitInt 2] 
+let t1 = teval Map.empty [] term1
+
+let term2 =
+    l "inlineable" 
+        (inl ["x";"y"] (T [V "x";V "y"]))
+        (T [(inap (V "inlineable") [LitInt 1; LitInt 2]); 
+            (inap (V "inlineable") [LitFloat 1.5; LitInt 2])])
+let t2 = teval Map.empty [] term2
+
+let term3 =
+    l "inlineable" 
+        (inl ["x";"y"] (T [V "x";V "y"]))
+        (l "fun" 
+            (inl ["inl";"a";"b";"c";"d"] 
+                (T [inap (V "inl") [V "a";V "b"];
+                    inap (V "inl") [V "c";V "d"]]))
+            (inap (V "fun") [V "inlineable"; LitInt 1; LitInt 2; LitFloat 1.5; LitInt 2]))
+let t3 = teval Map.empty [] term3
