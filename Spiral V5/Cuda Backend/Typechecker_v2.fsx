f47d0267f7621866392c7d23bf30fee0c1ad2df0
Marko Grdinić <mrakgr@gmail.com>
2017-03-01 16:00:41 +0100

    Functions are giving me trouble.
    I have all the pieces together, but unlike what I thought having even simple functions will require me to implement Algorithm W or something like that.
    
    No, it is not good.
    
    I'll stick to just macros and see where I can go with just that model.

--- Spiral V5/Cuda Backend/Typechecker_v2.fsx
+++ Spiral V5/Cuda Backend/Typechecker_v2.fsx
@@ -4,6 +4,7 @@
     | Float
     | Bool
     | Tuple of Ty list
+    | TArr of Ty list * Ty
 
 type Expr = 
     | V of string
@@ -16,7 +17,7 @@ type Expr =
     | LitBool of bool
     | T of Expr list // T stands for tuple
     | Method of string list * Expr
-    | Annotate of Expr * Expr list
+    | Apply of Expr * Expr list
 
 type TyV = int64 * string * Ty
 
@@ -29,9 +30,10 @@ type TypedExpr =
     | TyLitBool of bool
     | TyT of TypedExpr list
     | TyMethod of TyV list * TypedExpr * Ty
+    | TyApply of TypedExpr * TypedExpr list * Ty
 
 let rec get_type = function
-    | TyMethod(_,_,t) | TyV(_,_,t) | TyIf(_,_,_,t) | TyLet(_,_,_,t) -> t
+    | TyApply(_,_,t) | TyMethod(_,_,t) | TyV(_,_,t) | TyIf(_,_,_,t) | TyLet(_,_,_,t) -> t
     | TyLitInt _ -> Int
     | TyLitFloat _ -> Float
     | TyLitBool _ -> Bool
@@ -50,51 +52,66 @@ let get_tag =
         x <- x + 1L
         x'
 
-let rec teval (env: EnvType) inline_args (apply_args: (string list * EnvType) list) exp: ReturnCases =
+open System.Collections.Generic
+type Data =
+    {
+    // Immutable
+    env : EnvType
+    inline_args : (Expr list * EnvType) list
+    apply_args : (Expr list * EnvType) list
+    // Mutable
+    global_methods : Dictionary<int64 * Ty list,string * TypedExpr> // For hoisted out global methods.
+    }
+
+type Result<'a,'b> = Succ of 'a | Fail of 'b
+
+let d0() = {env=Map.empty;inline_args=[];apply_args=[];global_methods=Dictionary()}
+
+let rec teval (d: Data) exp: ReturnCases =
     match exp with
     | V x -> 
-        match Map.tryFind x env with
+        match Map.tryFind x d.env with
         | Some (RTypedExpr _ as v) -> v
-        | Some (RExpr(v,env)) -> teval env inline_args apply_args v
+        | Some (RExpr(v,env)) -> teval d v
         | Some (RError _ as e) -> e
         | None -> RError <| sprintf "Variable %A not bound." x
     | Inline(expr,args) ->
-        teval env ((args,env) :: inline_args) apply_args expr
+        teval {d with inline_args = (args,d.env) :: d.inline_args} expr
     | If(cond,tr,fl) as orig ->
-        match teval env inline_args apply_args cond with
+        match teval d cond with
         | RTypedExpr cond' when get_type cond' = Bool ->
-            match teval env inline_args apply_args tr, teval env inline_args apply_args fl with
+            match teval d tr, teval d fl with
             | RTypedExpr tr, RTypedExpr fl -> 
                 if get_type tr = get_type fl then
                     RTypedExpr <| TyIf(cond',tr,fl,get_type tr)
                 else
                     RError "Types in branches of if do not match."
-            | RExpr(_,_), RExpr(_,_) -> RExpr(orig,env)
+            | RExpr(_,_), RExpr(_,_) -> RExpr(orig,d.env)
             | a, b -> RError <| sprintf "Expected either both equal types or non-evaluated in conditional.\nGot true: %A\nGot false: %A" a b
         | x -> RError <| sprintf "Expected bool in conditional.\nGot: %A" x
     | Inlineable(args,body) as inl ->
-        match inline_args with
+        match d.inline_args with
         | (cur_args,env'') :: other_args ->
             if args.Length = cur_args.Length then
                 let env =
                     (args, cur_args) ||>
                     List.fold2 (fun env' arg cur_arg -> 
-                        Map.add arg (teval env'' [] apply_args cur_arg) env'
-                        ) env
-                teval env other_args apply_args body
+                        Map.add arg (teval {d with env=env''; inline_args=[]} cur_arg) env'
+                        ) d.env
+                teval {d with env=env; inline_args=other_args} body
             else
                 RError "args.Length = cur_args.Length failed in the Inlineable case"
-        | [] -> RExpr(inl,env)
+        | [] -> RExpr(inl,d.env)
     | Let(v,b,e) as orig ->
-        match teval env [] apply_args b with
+        match teval {d with inline_args=[]; apply_args=[]} b with
         | RTypedExpr b' ->
             let b'_type = get_type b'
             let v' = get_tag(),v,b'_type
-            match teval (Map.add v (RTypedExpr <| TyV(v')) env) inline_args apply_args e with
+            match teval {d with env=Map.add v (RTypedExpr <| TyV(v')) d.env} e with
             | RTypedExpr e' -> RTypedExpr(TyLet(v',b',e',get_type e'))
-            | RExpr(_,_) -> RExpr(orig,env)
+            | RExpr(e,_) -> RError <| sprintf "Got expression %A on the right side of a Let" e
             | RError er -> RError er
-        | RExpr _ as b -> teval (Map.add v b env) inline_args apply_args e
+        | RExpr _ as b -> teval {d with env=Map.add v b d.env} e
         | RError _ as e -> e
     | LitInt x -> RTypedExpr (TyLitInt x)
     | LitFloat x -> RTypedExpr (TyLitFloat x)
@@ -102,44 +119,35 @@ let rec teval (env: EnvType) inline_args (apply_args: (string list * EnvType) li
     | T ls as orig ->
         let rec loop acc = function
             | l::ls ->
-                match teval env inline_args apply_args l with
+                match teval d l with
                 | RTypedExpr x -> loop (x::acc) ls
-                | RExpr _ as x -> RExpr (orig, env)
+                | RExpr _ as x -> RExpr (orig, d.env)
                 | RError _ as x -> x
             | [] -> RTypedExpr (TyT (List.rev acc))
         loop [] ls
-    | Method(args,body) ->
-        match apply_args with
-        | (cur_args,env'') :: other_args ->
-            if args.Length = cur_args.Length then
-                let rec loop acc = function
-                    | l::ls ->
-                        match Map.tryFind l env'' with
-                        | Some(RTypedExpr _ as x) -> loop (Map.add l x acc) ls
-                        | _ -> None
-                    | [] -> Some acc
-                match loop Map.empty cur_args with
-                | Some env -> 
-                    let body = teval env [] [] body // This is a significant restriction...
-                    RTypedExpr(TyMethod(cur_args,body,get_type body))
-            else
-                RError "args.Length = cur_args.Length failed in the Method case"
-        | [] -> RError "No arguments to apply to the Method"
+
+//    | Method of string list * Expr
+//    | Apply of Expr * Expr list
+
+//    | TyMethod of TyV list * TypedExpr * Ty
+//    | TyApply of TypedExpr * TypedExpr list * Ty
 
 let inl x y = Inlineable(x,y)
 let inap x y = Inline(x,y)
 let l x b i = Let(x,b,i)
 
+let teval1 x = teval (d0()) x
+
 // Assorted tests
 let term1 = inap (inl ["x";"y"] (V "x")) [LitInt 1; LitInt 2] 
-let t1 = teval Map.empty [] term1
+let t1 = teval1 term1
 
 let term2 =
     l "inlineable" 
         (inl ["x";"y"] (T [V "x";V "y"]))
         (T [(inap (V "inlineable") [LitInt 1; LitInt 2]); 
             (inap (V "inlineable") [LitFloat 1.5; LitInt 2])])
-let t2 = teval Map.empty [] term2
+let t2 = teval1 term2
 
 let term3 =
     l "inlineable" 
@@ -149,13 +157,13 @@ let term3 =
                 (T [inap (V "inl") [V "a";V "b"];
                     inap (V "inl") [V "c";V "d"]]))
             (inap (V "fun") [V "inlineable"; LitInt 1; LitInt 2; LitFloat 1.5; LitInt 2]))
-let t3 = teval Map.empty [] term3
+let t3 = teval1 term3
 
 let term4 = 
     l "a" (LitInt 2)
         (l "b" (LitBool true)
             (T [V "a"; V "b"]))
-let t4 = teval Map.empty [] term4
+let t4 = teval1 term4
 
 let term5 = // If test
     l "if" (inl ["cond";"tr";"fl"] (If(V "cond",V "tr",V "fl")))
@@ -163,7 +171,7 @@ let term5 = // If test
             (l "tr" (LitFloat 3.33)
                 (l "fl" (LitFloat 4.44)
                     (inap (V "if") [V "cond";V "tr";V "fl"]))))
-let t5 = teval Map.empty [] term5
+let t5 = teval1 term5
 
 let term6 = // Error in conditional
     l "if" (inl ["cond";"tr";"fl"] (If(V "cond",V "tr",V "fl")))
@@ -171,7 +179,7 @@ let term6 = // Error in conditional
             (l "tr" (LitFloat 3.33)
                 (l "fl" (LitFloat 4.44)
                     (inap (V "if") [V "cond";V "tr";V "fl"]))))
-let t6 = teval Map.empty [] term6
+let t6 = teval1 term6
 
 let term7 = // Error in branches
     l "if" (inl ["cond";"tr";"fl"] (If(V "cond",V "tr",V "fl")))
@@ -179,7 +187,7 @@ let term7 = // Error in branches
             (l "tr" (LitInt 3)
                 (l "fl" (LitFloat 4.44)
                     (inap (V "if") [V "cond";V "tr";V "fl"]))))
-let t7 = teval Map.empty [] term7
+let t7 = teval1 term7
 
 let term8 = // Hygiene test
     l "f" 
@@ -192,4 +200,4 @@ let term8 = // Hygiene test
                 (l "b" (LitInt 4) (T [V "a"; V "b"]))))
             (inap (V "f") [V "g"]))
                 
-let t8 = teval Map.empty [] term8
+let t8 = teval1 term8
