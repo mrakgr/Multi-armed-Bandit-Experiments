23c63004d798cba7c29f6f1917a720673b8c6a71
Marko Grdinić <mrakgr@gmail.com>
2017-03-18 17:41:44 +0100

    This is where I will start. This particular typechecker will be solely to explore the feasibility of my new idea. The one I made has to do sequencing, macro expansion, hoisting and various other things during the typechecking phase.
    And it has to prepare the ground floor for the code generation phase.
    
    In contrast, I won't aim at code generation at all with this.
    
    I do not think that those other things are particularly difficult, but like the password thing today, they interfere with my thinking.
    
    V1 of the new typechecker will be here solely so I can hone my focus, laserlike on it – on that particular problem domain.

---
+++ Spiral V5/Cuda Backend/RecursiveTypechecker_v1a.fsx
@@ -0,0 +1,26 @@
+﻿type Expr = 
+    | If of Expr * Expr * Expr // can't do recursion without branching.
+    | Apply of Expr * Expr list
+    | Method of name: string * args: string list * body: Expr * else_: Expr
+    | Let of string * Expr * Expr
+
+    | LitUnit
+    | LitInt of int
+    | LitFloat of float
+    | LitBool of bool
+type Ty =
+    | UnitT
+    | Int32T
+    | Float32T
+    | BoolT
+and TyV = int64 * string * Ty
+and TypedExpr =
+    | TyV of TyV
+    | TyIf of TypedExpr * TypedExpr * TypedExpr * Ty
+    | TyLet of TyV * TypedExpr * TypedExpr * Ty
+    | TyMethodCall of name: string * TypedExpr list * Ty
+
+    | TyLitUnit
+    | TyLitInt of int
+    | TyLitFloat of float
+    | TyLitBool of bool
