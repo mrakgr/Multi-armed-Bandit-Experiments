31842d4ca160bcc86aa6dcaa9cb6567cfed27518
Marko Grdinić <mrakgr@gmail.com>
2017-04-02 12:08:22 +0200

    Before I can actually start on the library, I need to add two tedious features: Vg in the typechecker and SpiralCudaDeviceVar to replace CudaDeviceVar from ManagedCuda.
    I wish it were otherwise, but as it is now, I will make the Spiral library as a dynamic interpreted language. And the big idea will be to augment it with dynamically scoped functions by passing them through the environment.
    
    I'll use that in place of the template pattern.
    
    Focus, focus. I need to put in just a little bit work today in order to get on the swing of things.
    
    Let me do the two points above and after that, the rest won't be too hard. Only these two things will be tedious, the rest will be just fun.
    
    Fun with finally finishing the library and going into training RL agents.

---
+++ Spiral V5/V5 With The New Backend/probability.fsx
@@ -0,0 +1,24 @@
+﻿// Rewrite of the Probability Eff example with just functions.
+
+let rec uniform toss = function
+    | [] -> 0.0
+    | [x: int] -> float x
+    | x :: xs ->
+        let n = List.length xs + 1
+        let p = 1.0 / float n
+        toss p <| fun k -> if k then float x else uniform toss xs
+
+let w =
+    let toss p k = p * k true + (1.0 - p) * k false
+    let uniform = uniform toss
+    let x = uniform [1; 2; 3; 4; 5; 6] in
+    let y = uniform [1; 2; 3; 4; 5; 6] in
+    x + y
+
+// Note: No, I really can't add algebraic effects + handlers to the library.
+// That is because its control flow.
+
+// I am going to have to make do with simply extending the language with dynamically scoped functions and variables
+// by passing them through the environment.
+
+// That should make the new language expressive enough for my needs.
