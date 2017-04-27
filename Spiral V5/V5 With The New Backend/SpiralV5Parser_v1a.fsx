44d179aee8632f361fa9e065edaf8af820468cbd
Marko Grdinić <mrakgr@gmail.com>
2017-04-27 15:41:12 +0200

    I've started the thing, but it is definitely a bit complicated. The GVGAI parser had a lot of cases which made it difficult, but this one on the other hand is a lot more intricate.
    I need to think about it more before I can really start writting code.
    
    Let me save here as I need to step outside for a bit.

--- Spiral V5/V5 With The New Backend/SpiralV5Parser_v1a.fsx
+++ Spiral V5/V5 With The New Backend/SpiralV5Parser_v1a.fsx
@@ -6,13 +6,72 @@
 open SpiralV5CudaTypechecker_v7c'
 open FParsec
 
-let comma = spaces >>. skipChar ',' >>. spaces
-let standard_identifier = many1Satisfy isAsciiLetter |>> S
-let between_parenths p = between (skipChar '(') (skipChar ')') p
-let rec standard_pattern x = 
-    let standard_pattern = sepBy standard_pattern comma |> between_parenths |>> SS
-    (standard_identifier <|> standard_pattern) x
+let skipSpacesChar x = spaces >>. skipChar x
+let comma = skipSpacesChar ','
+let semicolon = skipSpacesChar ';'
+let eq = skipSpacesChar '='
+let lam x = 
+    spaces >>. skipString "->"
+    <| x
+let inl x = 
+    spaces >>. skipString "inl"
+    <| x
+let fun_ x = 
+    spaces >>. skipString "fun"
+    <| x
 
-let standard_patterns = sepEndBy1 standard_pattern spaces
+let identifier_template x = spaces >>. many1Satisfy isAsciiLetter |>> x 
+let pattern_identifier = identifier_template S
 
-run standard_patterns "add (a,b) = a + b;"
+let between_brackets l p r = between (skipSpacesChar l) (skipSpacesChar r) p
+let between_rounds p = between_brackets '(' p ')'
+let between_curlies p = between_brackets '{' p '}'
+let between_squares p = between_brackets '[' p ']'
+
+let rec pattern x = (spaces >>. (pattern_identifier <|> (sepBy (spaces >>. pattern) comma |> between_rounds |>> SS))) x
+
+let pattern_list = sepEndBy1 pattern spaces
+
+let pbool = (skipString "false" |>> fun _ -> LitBool false) <|> (skipString "true" |>> fun _ -> LitBool true)
+let puint32 = puint32 .>> skipChar 'u' |>> LitUInt32
+let pfloat32 = pfloat .>> skipChar 'f' |>> (float32 >> LitFloat32)
+let puint64 = puint64 .>> skipChar 'U' |>> LitUInt64
+let pint64 = pint64 .>> skipChar 'L' |>> LitInt64
+let pint32 = pint32 .>> notFollowedByString "."  |>> LitInt32
+let pfloat64 = pfloat |>> LitFloat64
+
+let plit = spaces >>. ([pbool;puint32;pfloat32;puint64;pint64;pint32;pfloat64] |> List.map attempt |> choice)
+let variable = identifier_template V
+let application = failwith ""
+
+let expr_body = spaces >>. ([plit; variable; application] |> List.map attempt |> choice)
+
+let expr_single = spaces >>. expr_body
+let expr_block = between_curlies (sepBy1 expr_single semicolon)
+let expr = spaces >>. (expr_block <|> expr_single)
+
+// There will be 7 basic patterns in the language.
+
+//1) pat = expr
+//2) name [pat] = expr
+//3) inl name [pat] = expr
+//4) inl [pat] → expr
+//5) fun name [pat] = expr
+//6) fun [pat] → expr
+//7) expr
+
+// #2 and #3 are equivalent to each other, but there rest are not. #4 and #6 are expressions
+// themselves and the `=` patterns are statements.
+
+let name = identifier_template id
+
+let case_pat = pipe2 pattern (eq >>. expr) (fun a b -> a)
+let case_name_pat_list = pipe3 name pattern (eq >>. expr) (fun a b c -> (a,b))
+let case_inl_name_pat = pipe3 (inl >>. name) pattern (eq >>. expr) (fun a b c -> (a,b))
+let case_inl_pat = pipe3 (inl >>. name) pattern (lam >>. expr) (fun a b c -> (a,b))
+let case_fun_name_pat = pipe3 (fun_ >>. name) pattern (eq >>. expr) (fun a b c -> (a,b))
+let case_fun_pat = pipe3 (fun_ >>. name) pattern (lam >>. expr) (fun a b c -> (a,b))
+let case_expr = expr
+
+run case_pat "(a,b) = a + b;"
+
