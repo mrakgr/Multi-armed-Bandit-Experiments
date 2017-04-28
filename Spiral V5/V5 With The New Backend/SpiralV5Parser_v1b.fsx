8b2fcf5e871d899646340318762a8065fea9b56e
Marko Grdinić <mrakgr@gmail.com>
2017-04-28 16:49:20 +0200

    I am hyped up. This is going really, really well. Right now I am thinking whether I should add indentation sensitive parsing as well.
    It would not be a big deal. When I was doing the GVGAI port I was much less experienced than now so it took me a long time to deal with it, but now I do not think that making it would be that big of a deal. The fact that I already got it finished in that port, is a bonus in this as well. I'll have to refactor it a bit, but it should work otherwise.
    
    Besides that I still need to add those operators.
    
    Well, let me stop outside for a bit first. The tuples are done to perfection.

---
+++ Spiral V5/V5 With The New Backend/SpiralV5Parser_v1b.fsx
@@ -0,0 +1,104 @@
+﻿#load "SpiralV5CudaTypechecker_v7c'.fsx"
+#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsecCS.dll"
+#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsec.dll"
+//#r "../../packages/FParsec-Pipes.0.3.1.0/lib/net45/FParsec-Pipes.dll"
+
+open SpiralV5CudaTypechecker_v7c'
+open FParsec
+
+type ParserExpr =
+    | ParserStatement of (CudaExpr -> CudaExpr)
+    | ParserExpr of CudaExpr
+
+let comma = skipChar ',' .>> spaces
+let semicolon = skipChar ';' .>> spaces
+let eq = skipChar '=' .>> spaces
+let lam = skipString "->" .>> spaces
+let inl_ = skipString "inl" .>> spaces
+let fun_ = skipString "fun" .>> spaces
+
+let identifier_template x = many1Satisfy isAsciiLetter .>> spaces |>> x 
+let between_brackets l p r = between (skipChar l .>> spaces) (skipChar r .>> spaces) p
+let between_rounds p = between_brackets '(' p ')'
+let between_curlies p = between_brackets '{' p '}'
+let between_squares p = between_brackets '[' p ']'
+
+let pattern_identifier = identifier_template S 
+let rec pattern_inner_template f = f pattern comma |>> SS
+and pattern_inner x = pattern_inner_template sepBy x 
+and pattern_inner1 x = pattern_inner_template sepBy1 x
+and pattern x = (pattern_identifier <|> between_rounds pattern_inner) x
+let pattern_list = sepEndBy1 pattern_inner1 spaces
+
+let pbool = (skipString "false" |>> fun _ -> LitBool false) <|> (skipString "true" |>> fun _ -> LitBool true)
+let puint32 = puint32 .>> skipChar 'u' |>> LitUInt32
+let pfloat32 = pfloat .>> skipChar 'f' |>> (float32 >> LitFloat32)
+let puint64 = puint64 .>> skipChar 'U' |>> LitUInt64
+let pint64 = pint64 .>> skipChar 'L' |>> LitInt64
+let pint32 = pint32 .>> notFollowedByString "."  |>> LitInt32
+let pfloat64 = pfloat |>> LitFloat64
+
+let plit = ([pbool;puint32;pfloat32;puint64;pint64;pint32;pfloat64] |> List.map attempt |> choice) .>> spaces
+let variable = identifier_template V
+
+let term = choice [plit; variable]
+
+// There will be 8 basic patterns in the language.
+
+// 1) pat = expr
+// 2) name [pat] = expr // Is not recursive.
+// 3) inl name [pat] = expr // Is recursive
+// 4) inl [pat] → expr
+// 5) fun name [pat] = expr // Is recursive
+// 6) fun [pat] → expr
+// 7) expr
+// 8) [pat] -> expr
+
+let name = identifier_template id
+
+let case_pat expr = pipe2 pattern_inner1 (eq >>. expr) (fun pattern body -> ParserStatement <| l pattern body)
+let case_name_pat_list expr = pipe3 name pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (inlr' "" pattern body))
+let case_inl_name_pat_list expr = pipe3 (inl_ >>. name) pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (inlr' name pattern body))
+let case_inl_pat_list expr = pipe2 (inl_ >>. pattern_list) (lam >>. expr) (fun pattern body -> ParserExpr <| inl' pattern body)
+let case_fun_name_pat_list expr = pipe3 (fun_ >>. name) pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (methr' name pattern body))
+let case_fun_pat_list expr = pipe2 (fun_ >>. pattern_list) (lam >>. expr) (fun pattern body -> ParserExpr <| meth' pattern body)
+let case_expr expr = expr |>> ParserExpr
+let case_pat_list expr = pipe2 pattern_list (lam >>. expr) (fun pattern body -> ParserExpr <| inl' pattern body)
+
+let cases_all expr =
+    let cases =
+        [case_inl_name_pat_list; case_inl_pat_list; case_fun_name_pat_list; case_fun_pat_list
+         case_pat; case_name_pat_list; case_pat_list; case_expr]
+        |> List.map (fun x -> x expr |> attempt)
+    spaces >>. choice cases
+
+let expr_block expr =
+    let process_parser_expr a b =
+        match a,b with
+        | ParserStatement a, ParserExpr b -> a b |> ParserExpr
+        | ParserExpr a, ParserExpr b -> l E a b |> ParserExpr
+        | _, ParserStatement _ -> failwith "Parser statements not allowed in the last position of a block. Expected an expression."
+    between_rounds (sepBy (cases_all expr) semicolon)
+    |>> function 
+        | _ :: _ as l -> 
+            List.reduceBack process_parser_expr l
+            |> function ParserExpr a -> a | _ -> failwith "Last member of a block must be an expression or the block must be empty. Got a statement instead."
+        | _ -> VV []
+
+let application expr =
+    many1 expr
+    |>> function x :: xs -> ap' x xs | _ -> failwith "impossible in application"
+
+let tuple expr =
+    sepBy1 expr comma
+    |>> function
+        | _ :: _ :: _ as l -> VV l
+        | x :: _ -> x
+        | _ -> failwith "Empty tuples are handled in expr_block."
+
+let rec expr x = 
+    let expr_body x = (choice [expr_block expr; term]) x
+    tuple (application expr_body) x
+
+run (expr) ""
+run (spaces >>. expr) "(q = (f -> 2) 3; q)"
