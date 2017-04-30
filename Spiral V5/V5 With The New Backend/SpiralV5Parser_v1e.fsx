8419b8553afdd01ec6eb3704c246a369ac0009d5
Marko Grdinić <mrakgr@gmail.com>
2017-04-30 19:36:54 +0200

    I finally got the indentation parser to work, tuples, operators and application. The changes were so simple that it took my breath a way for a moment and even I had to wonder how come I missed this.
    It is amazing that I have no need to track indentation in the UserState at all.
    
    It was a lot of struggle getting this going and I think I am going to stop here so I can end the day on a high note.
    
    I tried adding semicolons as well, but it is not that simple. I am going to have to spend some energy to figure it out and I do not have that anymore.
    
    I'll put the finishing touches on this tomorrow and then plug it into the compiler.
    
    Once I do that, writting those kernels will get a lot easier.

--- Spiral V5/V5 With The New Backend/SpiralV5Parser_v1e.fsx
+++ Spiral V5/V5 With The New Backend/SpiralV5Parser_v1e.fsx
@@ -11,7 +11,7 @@ type ParserExpr =
     | ParserExpr of CudaExpr
 
 let comma = skipChar ',' .>> spaces
-let semicolon x = (skipChar ';' .>> spaces) x
+let semicolon = skipChar ';' .>> spaces
 let eq = skipChar '=' .>> spaces
 let lam = skipString "->" .>> spaces
 let inl_ = skipString "inl" .>> spaces
@@ -21,20 +21,21 @@ let fun_rec = skipString "fun" .>> spaces .>> skipString "rec" .>> spaces
 
 let identifier_template = 
     many1Satisfy2L isAsciiLetter (fun x -> isAsciiLetter x || isDigit x || x = ''') "identifier" .>> spaces
-    >>= function
-        | "inl" | "fun" as x -> fun _ -> Reply(Error,messageError <| sprintf "%s not allowed as an identifier." x)
+    >>=? function
+        | "rec" | "if" | "then" | "else" | "inl" | "fun" as x -> fun _ -> 
+            Reply(Error,messageError <| sprintf "%s not allowed as an identifier." x)
         | x -> preturn x
         
 let between_brackets l p r = between (skipChar l .>> spaces) (skipChar r .>> spaces) p
-let between_rounds p = between_brackets '(' p ')'
-let between_curlies p = between_brackets '{' p '}'
-let between_squares p = between_brackets '[' p ']'
+let rounds p = between_brackets '(' p ')'
+let curlies p = between_brackets '{' p '}'
+let quares p = between_brackets '[' p ']'
 
 let pattern_identifier = identifier_template |>> S 
 let rec pattern_inner_template f = f pattern comma |>> SS
 and pattern_inner x = pattern_inner_template sepBy x 
 and pattern_inner1 x = pattern_inner_template sepBy1 x
-and pattern x = (pattern_identifier <|> between_rounds pattern_inner) x
+and pattern x = (pattern_identifier <|> rounds pattern_inner) x
 let pattern_list = sepEndBy1 pattern_inner1 spaces
 
 let pbool = (skipString "false" |>> fun _ -> LitBool false) <|> (skipString "true" |>> fun _ -> LitBool true)
@@ -45,59 +46,73 @@ let pint64 = pint64 .>> skipChar 'L' |>> LitInt64
 let pint32 = pint32 .>> notFollowedByString "."  |>> LitInt32
 let pfloat64 = pfloat |>> LitFloat64
 
-let plit = ([pbool;puint32;pfloat32;puint64;pint64;pint32;pfloat64] |> List.map attempt |> choice) .>> notFollowedBy asciiLetter .>> spaces
-let variable_or_if_then_else expr =
-    identifier_template 
-    >>=? function
-        | "if" -> 
-            pipe3
-                expr
-                (skipString "then" >>. spaces1 >>. expr)
-                (skipString "else" >>. spaces1 >>. expr)
-                (fun cond tr fl -> If(cond,tr,fl))
-        | "then" | "else" as x -> 
-            fun _ -> Reply(Error, messageError <| sprintf "variable cannot be named one of: then, else.\nGot: %s" x)
-        | x -> fun _ -> Reply(V x)
-            
-let term expr = plit <|> variable_or_if_then_else expr
-let rounds expr = between_rounds expr
-
-// Language cases.
-
-// 1a) inl pat = expr
-// 2a) inl name [pat] = expr 
-// 3a) inl rec name [pat] = expr
-// 4a) fun name [pat] = expr 
-// 5a) fun rec name [pat] = expr
-// 1b) inl pat -> expr
-// 3b) inl rec name [pat] -> expr
-// 5b) fun rec name [pat] -> expr
-// 6) (expr)
-// 7) term
+let lit = ([pbool;puint32;pfloat32;puint64;pint64;pint32;pfloat64] |> List.map attempt |> choice) .>> notFollowedBy asciiLetter .>> spaces
+let var = identifier_template |>> V
+let if_then_else expr =
+    pipe3
+        (skipString "if" >>. spaces1 >>. expr)
+        (skipString "then" >>. spaces1 >>. expr)
+        (skipString "else" >>. spaces1 >>. expr)
+        (fun cond tr fl -> If(cond,tr,fl))
 
 let name = identifier_template
 
-let case_inl_pat_statement expr = pipe2 (inl_ >>. pattern_inner1) (eq >>. expr) (fun pattern body -> ParserStatement <| l pattern body)
-let case_inl_name_pat_list_statement expr = pipe3 (inl_ >>. name) pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (inlr' "" pattern body))
-let case_inl_rec_name_pat_list_statement expr = pipe3 (inl_rec >>. name) pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (inlr' name pattern body))
-let case_fun_name_pat_list_statement expr = pipe3 (fun_ >>. name) pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (methr' "" pattern body))
-let case_fun_rec_name_pat_list_statement expr = pipe3 (fun_rec >>. name) pattern_list (eq >>. expr) (fun name pattern body -> ParserStatement <| l (S name) (methr' name pattern body))
+let case_inl_pat_statement expr = pipe2 (inl_ >>. pattern_inner1) (eq >>. expr) (fun pattern body -> l pattern body)
+let case_inl_name_pat_list_statement expr = pipe3 (inl_ >>. name) pattern_list (eq >>. expr) (fun name pattern body -> l (S name) (inlr' "" pattern body))
+let case_inl_rec_name_pat_list_statement expr = pipe3 (inl_rec >>. name) pattern_list (eq >>. expr) (fun name pattern body -> l (S name) (inlr' name pattern body))
+let case_fun_name_pat_list_statement expr = pipe3 (fun_ >>. name) pattern_list (eq >>. expr) (fun name pattern body -> l (S name) (methr' "" pattern body))
+let case_fun_rec_name_pat_list_statement expr = pipe3 (fun_rec >>. name) pattern_list (eq >>. expr) (fun name pattern body -> l (S name) (methr' name pattern body))
 
-let case_inl_pat_expr expr = pipe2 (inl_ >>. pattern_inner1) (lam >>. expr) (fun pattern body -> ParserExpr <| inl pattern body)
-let case_inl_rec_name_pat_list_expr expr = pipe3 (inl_rec >>. name) pattern_list (lam >>. expr) (fun name pattern body -> ParserExpr <| inlr' name pattern body)
-let case_fun_rec_name_pat_list_expr expr = pipe3 (fun_rec >>. name) pattern_list (lam >>. expr) (fun name pattern body -> ParserExpr <| methr' name pattern body)
-
-let case_term expr = term expr |>> ParserExpr
-let case_rounds expr = rounds expr |>> ParserExpr
-
-let cases_all expr =
-    [case_inl_pat_statement; case_inl_name_pat_list_statement; case_inl_rec_name_pat_list_statement; 
-     case_inl_name_pat_list_statement; case_inl_rec_name_pat_list_statement; case_inl_pat_expr; 
-     case_inl_rec_name_pat_list_expr; case_inl_rec_name_pat_list_expr; case_term; case_rounds]
+let statements expr = 
+    [case_inl_pat_statement; case_inl_name_pat_list_statement; case_inl_rec_name_pat_list_statement
+     case_fun_name_pat_list_statement; case_fun_rec_name_pat_list_statement]
     |> List.map (fun x -> x expr |> attempt)
-    |> choice 
+    |> choice
 
-let expr_block expr =
+let case_inl_pat_expr expr = pipe2 (inl_ >>. pattern_inner1) (lam >>. expr) (fun pattern body -> inl pattern body)
+let case_inl_rec_name_pat_list_expr expr = pipe3 (inl_rec >>. name) pattern_list (lam >>. expr) (fun name pattern body -> inlr' name pattern body)
+let case_fun_rec_name_pat_list_expr expr = pipe3 (fun_rec >>. name) pattern_list (lam >>. expr) (fun name pattern body -> methr' name pattern body)
+
+let case_plit expr = lit
+let case_if_then_else expr = if_then_else expr 
+let case_rounds expr = rounds expr 
+let case_var expr = var
+
+let expressions expr =
+    [case_inl_pat_expr; case_inl_rec_name_pat_list_expr; case_fun_rec_name_pat_list_expr
+     case_plit; case_if_then_else; case_rounds; case_var]
+    |> List.map (fun x -> x expr |> attempt)
+    |> choice
+   
+let process_parser_exprs exprs = 
+    let error_statement_in_last_pos _ = Reply(Error,messageError "Statements not allowed in the last position of a block.")
+    let process_parser_expr a b on_fail on_succ =
+        match a,b with
+        | ParserStatement a, ParserExpr b -> a b |> ParserExpr |> on_succ
+        | ParserExpr a, ParserExpr b -> l E a b |> ParserExpr |> on_succ
+        | _, ParserStatement _ -> on_fail error_statement_in_last_pos
+
+    let process_parser_exprs l =
+        let rec loop = function
+            | b :: a :: xs ->
+                process_parser_expr a b 
+                    id
+                    (fun r -> loop (r :: xs))
+            | x :: xs ->
+                match x with
+                | ParserExpr a -> preturn a 
+                | ParserStatement a -> error_statement_in_last_pos
+            | [] -> failwith "impossible"
+        loop (List.rev l)
+
+    match exprs with
+    | _ :: _ -> process_parser_exprs exprs
+    | _ -> preturn (VV [])
+
+let expr_alternate alternator statements expressions = 
+    alternator ((statements |>> ParserStatement) <|> (expressions |>> ParserExpr)) >>= process_parser_exprs
+
+let indentations statements expressions =
     let expr_indent expr (s: CharStream<_>) =
         let i = s.Column
         let expr (s: CharStream<_>) = 
@@ -105,43 +120,17 @@ let expr_block expr =
             else pzero s
         many1 expr s
 
-    let error er _ = Reply(Error,messageError er)
-    let error_statement_in_last_pos_msg = "Statements not allowed in the last position of a block."
-    let process_parser_expr a b on_fail on_succ =
-        match a,b with
-        | ParserStatement a, ParserExpr b -> a b |> ParserExpr |> on_succ
-        | ParserExpr a, ParserExpr b -> l E a b |> ParserExpr |> on_succ
-        | _, ParserStatement _ -> on_fail error_statement_in_last_pos_msg
+    expr_alternate expr_indent statements expressions
 
-    let process_parser_exprs l =
-        let rec loop = function
-            | b :: a :: xs ->
-                process_parser_expr a b 
-                    error
-                    (fun r -> loop (r :: xs))
-            | x :: xs ->
-                match x with
-                | ParserExpr a -> preturn a 
-                | ParserStatement a -> error error_statement_in_last_pos_msg
-            | [] -> failwith "impossible"
-        loop (List.rev l)
-    
-    let process_fin = function
-        | _ :: _ as l -> process_parser_exprs l
-        | _ -> preturn (VV [])
-
-    let cases x = cases_all expr x
-
-    expr_indent cases >>= process_fin
+let semicolons statements expressions = 
+    expr_alternate (fun x -> sepBy x semicolon) statements expressions
     
 let application expr (s: CharStream<_>) =
     let i = s.Column
     let expr_up (s: CharStream<_>) = 
         if i < s.Column then expr s
         else pzero s
-    (pipe2 expr (many expr_up) (fun x xs -> 
-        printfn "I am in application. x=%A\nxs=%A" x xs
-        ap' x xs)) s
+    pipe2 expr (many expr_up) (fun x xs -> ap' x xs) s
 
 let tuple expr =
     sepBy1 expr comma
@@ -168,22 +157,28 @@ let expr =
     opp.AddOperator(InfixOperator("||", spaces, 2, Associativity.Right, fun x y -> And(x,y)))
     opp.AddOperator(InfixOperator("&&", spaces, 3, Associativity.Right, fun x y -> Or(x,y)))
 
-    let expr = tuple opp.ExpressionParser
-    opp.TermParser <- expr_block expr
-    //opp.TermParser <- application (expr_block expr)
-    
+    let operators expr =
+        let p = opp.ExpressionParser
+        opp.TermParser <- expr
+        p
+
+    let rec expr s = 
+//        semicolons
+//            (statements expr)
+            (tuple (indentations (statements expr) (operators (application (expressions expr)))))
+                s
+
     expr
-
-let test = 
+    
+let test2 = 
     """
-    inl add (x,y) = 
-        inl q = x
-        inl w = y
-        (q+w,q*w,q-w)
+    inl w = 
+        function if true then q else f
+            2
+            3
     1
-    2
-    3
     """
 
-let result = runParserOnString (spaces >>. expr) -1L "" test
+let test = "a,(b + f e, 2, 3),c"
 
+let result = run (spaces >>. expr) test2
