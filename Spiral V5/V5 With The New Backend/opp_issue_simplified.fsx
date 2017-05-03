5263f92242fae99627684121870a19cb52cb4a72
Marko Grdinić <mrakgr@gmail.com>
2017-05-03 11:16:40 +0200

    No, I can't possibly show the full parser. Here is a simplified example.

---
+++ Spiral V5/V5 With The New Backend/opp_issue_simplified.fsx
@@ -0,0 +1,40 @@
+﻿#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsecCS.dll"
+#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsec.dll"
+
+open FParsec
+
+type Expr = 
+    | V of string
+    | Add of Expr * Expr
+
+let identifier = many1Satisfy2L isAsciiLetter (fun x -> isAsciiLetter x || isDigit x || x = ''') "identifier" .>> spaces |>> V
+
+let indentations expressions (s: CharStream<_>) =
+    let i = s.Column
+    let expr_indent expr (s: CharStream<_>) =
+        let expr (s: CharStream<_>) = if i <= s.Column then expr s else pzero s
+        many1 expr s
+
+    expr_indent (expressions i) s
+
+let expr =
+    let opp = new OperatorPrecedenceParser<_,_,_>()
+    opp.AddOperator(InfixOperator("+", spaces, 6, Associativity.Left, fun x y -> Add(x,y)))
+
+    let operators expr i =
+        let f expr (s: CharStream<_>) = if i <= s.Column then expr s else pzero s
+        opp.TermParser <- f expr
+        f opp.ExpressionParser
+
+    let rec expr s = indentations (operators identifier) s
+
+    expr
+
+let test2 = // I'd like this to be an error.
+    """
+    a 
+   + 
+    b 
+    """
+
+let result = run (spaces >>. expr) test2
