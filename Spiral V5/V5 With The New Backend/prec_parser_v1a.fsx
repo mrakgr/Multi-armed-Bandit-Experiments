89f8569090b344a3f9bbe60d0076e437ac9d55af
Marko Grdinić <mrakgr@gmail.com>
2017-05-19 19:33:49 +0200

    Today I spent the entire day just reading other people's code. That sucked.
    I managed to find some stuff in F# on TDOP parsing, but by far
    
    http://eli.thegreenplace.net/2010/01/02/top-down-operator-precedence-parsing
    
    this particular Python example was the most illuminating.
    
    I usually waste a lot of time building up the basic intutiion when I try new things like this. I am going to try harder tomorrow and instead of trying to think things through, leave the thoughts of Spiral behind and just try making the parser.
    
    I need to settle upon an approach. I already had an idea in mind before I started studying this TDOP stuff.
    
    I will get this done and move on to more interesting things.

---
+++ Spiral V5/V5 With The New Backend/prec_parser_v1a.fsx
@@ -0,0 +1,6 @@
+﻿#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsecCS.dll"
+#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsec.dll"
+
+open FParsec
+ 
+// TODO: Give the TDOP parser a try tomorrow.
