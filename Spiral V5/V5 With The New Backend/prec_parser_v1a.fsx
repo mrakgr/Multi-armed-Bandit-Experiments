9eef6d518ff985f21cefb94cc38eb772b351cb62
Marko Grdinić <mrakgr@gmail.com>
2017-05-20 18:18:45 +0200

    I finally bit the bullet and wrote some code. As horribly styled as it is, I am surprised that this works.
    I will refactor and play with it tomorrow, I am too tired right now. The section towards the end on the bad parts of Javascript was interesting to me as a language designer. It seems just by following F#'s basic design in my own language, I've managed to avoid all of those pitfalls.
    
    It is amazing what ridiculous scoping and typing issues Javascript has. And right now it is the most popular language in the world by far.
    
    and expression rbp =
        let t = token()
        advance()
        let mutable left = nud t
        while rbp < bp (token()) do
            let t = token()
            advance()
            left <- led t left
        left
    
    But looking at how this came out in particular, I am definitely skeptical about its good parts.

--- Spiral V5/V5 With The New Backend/prec_parser_v1a.fsx
+++ Spiral V5/V5 With The New Backend/prec_parser_v1a.fsx
@@ -1,26 +1,56 @@
-﻿#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsecCS.dll"
-#r "../../packages/FParsec.1.0.2/lib/net40-client/FParsec.dll"
-
-open FParsec
- 
-type Token =
+﻿type Token =
 | Num of int
 | Add
 | Sub
 | Mult
 | Div
+| Stop
 
+type Expr =
+| ENum of int
+| EAdd of Expr * Expr
+| ESub of Expr * Expr
+| EMult of Expr * Expr
+| EDiv of Expr * Expr
+| EStop
 
-let prec = function
+let bp = function
+    | Stop -> 0
     | Num x -> 1
     | Add -> 50
     | Sub -> 50
     | Mult -> 60
     | Div -> 60
 
-let is_nub = function
-    | Num _ -> true
-    | _ -> false
+let test = [|Num 2; Mult; Num 3; Add; Num 1; Stop|]
 
-let test = [|Num 1; Add; Num 2; Mult; Num 3|]
+let tokens = test
+let mutable token_idx = 0
+let token() = tokens.[token_idx]
+let advance() = token_idx <- token_idx + 1
 
+let rec led token left =
+    let bp = bp token
+    match token with
+    | Add -> EAdd(left, expression bp)
+    | Sub -> ESub(left, expression bp)
+    | Mult -> EMult(left, expression bp)
+    | Div -> EDiv(left, expression bp)
+
+and nud token = 
+    let bp = bp token
+    match token with
+    | Stop -> EStop
+    | Num x -> ENum x
+
+and expression rbp =
+    let t = token()
+    advance()
+    let mutable left = nud t 
+    while rbp < bp (token()) do
+        let t = token()
+        advance()
+        left <- led t left
+    left
+
+let t = expression 0
