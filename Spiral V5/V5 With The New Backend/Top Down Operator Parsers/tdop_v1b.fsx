bd1e324c92cd35ad19f880e2a8ca444854dd2d94
Marko Grdinić <mrakgr@gmail.com>
2017-05-21 18:48:44 +0200

    Today was strange for me. Basically, for the first 4 hours since I got up instead of programming, I just socialized on Reddit. It was on programming languages, but still it is unlike me. Those few posts that I made took a lot of energy out of me. And then after I finished breakfast, instead of getting back into it, I started watching talks by Douglas Crockford.
    That guy is quite a good speaker.
    
    https://www.youtube.com/watch?v=6eOhyEKUJko
    Douglas Crockford - Which way is forward
    
    His parser talk was good, and this one did strike a chord with me as well. It is on paradigm shifts and how they happen. I never even wondered about binary floating point numbers until now.
    
    I am going to try and do at least some programming today. Before I got sidetracked with Reddit I refactored the thing I did yesterday.
    
    ```
    and expression rbp s =
        let inline comb parser token s = parser (token s) s
        let rec loop (left: Expr) = if rbp < comb bp peek s then comb (led left) token s |> loop else left
        comb nud token s |> loop
    ```
    
    This single piece is in fact expremely brilliant. I am trying to think of how I would represent it using the usual functional combinators and am coming up empty.
    
    Edit: I forgot to push the above earlier.
    
    At any rate, now that I've played with this for an hour or so, I feel like I understand it completely.
    
    Seriously, of all the parser techniques Douglas Crockford could pick to hype up, he sure picked the right one.
    
    Right now I am too tired to start anything new, but what I am going to do tomorrow is start the parser from scratch.
    
    I am going to repurpose Fparsec to just do tokenizing and then write a fresh new top down parser just for the Spiral language. It deserves at least that much.
    
    Actually, this particular technique will be a marked improvement in efficiency compared to what I had before.
    
    Even excepting all the object allocations and non-inlined higher order functions, the parser is doing a ridiculous amount of backtracking.
    
    If I need to do that sort of thing, it would be far better to lex the language first.

---
+++ Spiral V5/V5 With The New Backend/Top Down Operator Parsers/tdop_v1b.fsx
@@ -0,0 +1,67 @@
+﻿type Token =
+| Num of int
+| Add
+| Sub
+| Mult
+| Div
+| Pow
+| Stop
+
+type Expr =
+| ENum of int
+| EAdd of Expr * Expr
+| ESub of Expr * Expr
+| EMult of Expr * Expr
+| EDiv of Expr * Expr
+| EPow of Expr * Expr
+| ENegate of Expr
+| EStop
+
+let bp x s = 
+    match x with
+    | Stop -> 0
+    | Num x -> 1
+    | Add -> 50
+    | Sub -> 50
+    | Mult -> 60
+    | Div -> 60
+    | Pow -> 70
+
+let test1 = [|Num 2; Mult; Num 3; Add; Num 1; Stop|]
+let test2 = [|Num 5; Div; Num 2; Pow; Num 3; Pow; Num 4; Stop|]
+let test3 = [|Sub; Sub; Num 5; Div; Num 2; Pow; Num 3; Pow; Num 4; Stop|]
+
+type ParserState =
+    {
+    mutable token_idx: int
+    tokens: Token[]
+    }
+
+let s0 x = {token_idx=0; tokens=x}
+let peek (s: ParserState) = s.tokens.[s.token_idx]
+let token (s: ParserState) = 
+    let t = peek s
+    s.token_idx <- s.token_idx + 1
+    t
+
+let rec led left token s =
+    let bp = bp token s
+    match token with
+    | Add -> EAdd(left, expression bp s)
+    | Sub -> ESub(left, expression bp s)
+    | Mult -> EMult(left, expression bp s)
+    | Div -> EDiv(left, expression bp s)
+    | Pow -> EPow(left, expression (bp-1) s)
+
+and nud token s = 
+    match token with
+    | Sub -> ENegate (expression 100 s)
+    | Stop -> EStop
+    | Num x -> ENum x
+
+and expression rbp s =
+    let inline comb parser token s = parser (token s) s
+    let rec loop (left: Expr) = if rbp < comb bp peek s then comb (led left) token s |> loop else left
+    comb nud token s |> loop
+    
+let t = expression 0 (s0 test3)
