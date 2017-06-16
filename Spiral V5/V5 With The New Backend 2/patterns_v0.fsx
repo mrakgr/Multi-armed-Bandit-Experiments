a470b6ef2caaa897659fa8471d120dfdc4a3b140
Marko Grdinić <mrakgr@gmail.com>
2017-06-16 15:05:35 +0200

    The chapters on pattern matching were remarkably brief so I will be able to finish the rest of the book today. After that it will be time to start programming.
    I will start by translating the pattern matching chapters to F# from Miranda.
    
    Right now I still do not feel like starting. I want to skim the rest of the book and am prepping my brain to start.
    
    The key is to simplify what is to come as much as possible. I'll do pattern matching naively and I will make types dynamically scoped. The main goal is to make the static and dynamic parts flow seamlessly together.
    
    Only after that will I start playing around with partial evaluation.
    
    Maybe I will do a bunch of HaskerRank exercises, this time in Spiral.
    
    To experiment I really need to make the F# backend, though I might do plain C as well just for kicks.
    
    Right now the language has pattern matching already, but it is only a compile time feature.
    
    Once I make this work properly at runtime, I will have completed the Spiral language and the world is my oyster.

---
+++ Spiral V5/V5 With The New Backend 2/patterns_v0.fsx
@@ -0,0 +1,11 @@
+﻿let tack x xss = (x :: List.head xss) :: List.tail xss
+
+let rec partition f = function
+    | [] -> []
+    | [x] -> [[x]]
+    | (x::x'::xs) ->
+        if f x = f x' then tack x (partition f (x'::xs))
+        else [x] :: partition f (x'::xs)
+
+let odd x = x % 2 = 1
+let t1 = partition odd [1;3;2;4;1]
