fc232ef33e15141054e77904ffa9da6db4e689c2
Marko Grdinić <mrakgr@gmail.com>
2017-06-18 15:07:48 +0200

    ``` and TypedPattern =     | PatS of TyV     | PatR of TypedPattern list * TypedPattern option     | PatF of TypedPattern * TypedExpr     | PatWhen of TypedPattern * TypedExpr ```
    A bit by bit, I am separating the task into relevant pieces. Written out as above, it does not seem like it would require much insight, but it did take me quite a bit of thinking to come to.
    
    Somewhere along the way it did slip my mind that I do not have to do everything in the partial evaluator. I can just check for which patterns are valid and assign proper variables to them.
    
    Once I do that, I've already done half the pattern matching job and if I want to I can translate them to F# directly or naively to C.
    
    As a programmer, I always go through the same pattern where I try to carry too large of a burden and it stresses me. The end result of that is always simplification. I keep looking for ways to lighten up and I find them.
    
    At the end of suffering lies inspiration.
    
    https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/video-lectures/4a-pattern-matching-and-rule-based-substitution/
    
    Now that I've gotten that innovation down, I still feel like reading a bit more on pattern matching compilation. I was not aware that there was stuff on this in the SICP book. I must have missed it while skimming the thing. The above is video from the course.
    
    Instead trying to wrack my brain on how I am going to deal with Pattern and such, reasoning about TypedPattern in comparison will be 10x easier and will allow me to make headway in my understanding of pattern matching algorithms.
    
    Once I have a matcher in such a form, the only thing left that remains is to merge the patterns together in addition to reordering them.

--- Spiral V5/V5 With The New Backend 2/SpiralV5Language_v10a.fsx
+++ Spiral V5/V5 With The New Backend 2/SpiralV5Language_v10a.fsx
@@ -49,7 +49,136 @@ and Pattern =
     | N of string * Pattern // Matches a tuple name and proceeds onto the pattern on a hit.
     | C of string * Pattern // Matches a type constructor name and proceeds onto the pattern on a hit.
     | Or of Pattern * Pattern
+    | When of Pattern * Expr
 
-let x = function
-    | a :: b :: c :: d -> 1
-    | a -> 2
+and TypedPattern =
+    | PatS of TyV
+    | PatR of TypedPattern list * TypedPattern option
+    | PatF of TypedPattern * TypedExpr
+    | PatWhen of TypedPattern * TypedExpr
+
+and Value = 
+    | LitUInt8 of uint8
+    | LitUInt16 of uint16
+    | LitUInt32 of uint32
+    | LitUInt64 of uint64
+    | LitInt8 of int8
+    | LitInt16 of int16
+    | LitInt32 of int32
+    | LitInt64 of int64
+    | LitFloat32 of float32
+    | LitFloat64 of float
+    | LitBool of bool
+    | LitString of string
+
+    | ThreadIdxX | ThreadIdxY | ThreadIdxZ
+    | BlockIdxX | BlockIdxY | BlockIdxZ
+
+and Op =
+    // TriOps
+    | If
+
+    // BinOps
+    | Add
+    | Sub
+    | Mult 
+    | Div 
+    | Mod 
+    | LTE 
+    | LT 
+    | EQ 
+    | NEQ 
+    | GT 
+    | GTE 
+    | And 
+    | Or 
+    | MSet 
+
+    | Apply
+    | ApplyType
+    | ApplyModule
+    | MethodMemoize
+    | StructCreate
+    | VVIndex
+    | VVCons
+    | TypeAnnot
+    | ModuleWith
+    | ModuleWith'
+    | EnvUnseal
+    | TypeSeal
+
+    | ArrayCreate
+    | ArrayCreateShared
+    | ArrayIndex
+    | ArrayUnsafeIndex
+   
+    | ShiftLeft
+    | ShiftRight
+    | ShuffleXor
+    | ShuffleUp
+    | ShuffleDown
+    | ShuffleIndex
+
+    // Static unary operations
+    | StaticPrint
+    | ErrorNonUnit
+    | ErrorType
+    | ModuleOpen
+
+    // UnOps
+    | Neg
+    | Log
+    | Exp
+    | Tanh
+
+    // Constants
+    | ModuleCreate
+
+    | Syncthreads
+    | BlockDimX | BlockDimY | BlockDimZ
+    | GridDimX | GridDimY | GridDimZ
+
+and PosKey = string * int64 * int64
+and Pos = PosKey option
+
+and Expr = 
+    | V of string * Pos
+    | T of TypedExpr * Pos
+    | Lit of Value * Pos
+    | Function of FunctionCore * Set<string> ref * Pos
+    | VV of Expr list * string * Pos // named tuple
+    | Op of Op * Expr list * Pos
+
+and Arguments = Set<TyV> ref
+and Renamer = Map<Tag,Tag>
+
+and MemoExprType =
+| MemoClosure
+| MemoMethod
+
+and LetType =
+| LetStd
+| LetInvisible
+
+and TypedExpr =
+    | TyType of Ty
+    | TyV of TyV
+    | TyLet of LetType * TyV * TypedExpr * TypedExpr * Ty
+    | TyLit of Value
+    
+    | TyVV of TypedExpr list * Ty
+    | TyEnv of EnvTerm * Ty
+    | TyOp of Op * TypedExpr list * Ty
+    | TyMemoizedExpr of MemoExprType * Arguments * Renamer * Tag * Ty
+    | TyMatch of TypedPattern * Ty
+
+and MemoCases =
+    | MethodInEvaluation
+    | MethodDone of TypedExpr
+// This key is for functions without arguments. It is intended that the arguments be passed in through the Environment.
+and MemoDict = Dictionary<MemoKey, MemoCases * Tag * Arguments>
+and ClosureDict = Dictionary<Tag, TypedExpr> 
+
+type Result<'a,'b> = Succ of 'a | Fail of 'b
+
+let flip f a b = f b a
