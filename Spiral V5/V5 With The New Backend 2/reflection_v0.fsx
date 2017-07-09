5943cf35fb2f7b072a3d2b48b38d27c8188c4fd4
Marko Grdinić <mrakgr@gmail.com>
2017-07-09 11:13:27 +0200

    I did quite a bit of experimentation with .NET reflection just now. I figured out how to intantiate the type properly too.
    With this, everything is set for me to make a .NET interface for my language. I won't allow creating .NET typees inside it, but I will allow using them.
    
    Hmmmm...is there anything I am missing here?
    
    Well, as is usual, after I have had a nice knowledge meal, it is best to rest a bit so it settles down in the mind.
    
    So I will do that here.
    
    The truth is, I feel like I still need to do more research on dynamic languages

--- Spiral V5/V5 With The New Backend 2/reflection_v0.fsx
+++ Spiral V5/V5 With The New Backend 2/reflection_v0.fsx
@@ -3,12 +3,28 @@ open System.Reflection
 
 let x = Reflection.Assembly.Load("mscorlib")
 let array_type = x.GetType("System.Array")
+
 let methods = array_type.GetMethods()
 let method_types (x: MethodInfo) = x.GetParameters(), x.ReturnParameter
 let param = methods |> Array.map (fun x -> x.Name, method_types x)
 let members = array_type.GetMembers()
-let o = array_type.InvokeMember("CreateInstance",BindingFlags.Default,null,null,[|typeof<int>,[|10|]|])
+let o_args: obj[] = [|typeof<int>;[|10|]|]
+let o = array_type.InvokeMember("CreateInstance",BindingFlags.InvokeMethod,null,null,o_args)
+
 
 let ar = 10
-let cr = array_type.GetMethod("CreateInstance",[|typeof<int>;ar.GetType()|])
+let ct_tys = [|typeof<int>.GetType();ar.GetType()|]
+let cr = array_type.GetMethod("CreateInstance",ct_tys)
+cr
 
+let strb_ty = typeof<System.Text.StringBuilder>
+let args: obj[] = [|"Qwe"; 128|]
+let strb = Activator.CreateInstance(strb_ty,args)
+strb
+
+open System.Collections.Generic
+let dic = typeof<Dictionary<int,int>>
+let dic' = x.GetType("System.Collections.Generic.Dictionary`2")
+let dic_ins_typ = dic'.MakeGenericType [|typeof<int>;typeof<int>|]
+let dic_ins = Activator.CreateInstance(dic_ins_typ)
+dic_ins_typ.InvokeMember("Add",BindingFlags.InvokeMethod,null,dic_ins,[|0;100|])
