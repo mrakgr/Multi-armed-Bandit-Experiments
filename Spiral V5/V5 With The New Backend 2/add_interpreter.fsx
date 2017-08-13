c24092dc94f5d31befe175334d12c73adf5b219f
Marko Grdinić <mrakgr@gmail.com>
2017-08-13 18:20:08 +0200

    https://stackoverflow.com/questions/45662291/why-does-deep-usage-of-the-stack-cause-superlinear-time-behavior-for-a-simple-in
    I opened this question on SO. I'll see what answer I get later. Tomorrow my focus will be to get better behavior on my traversal. I am going to have to be more careful where I put destructure.
    
    I am really going to have to do my utmost to reduce stack usage in the compiler for the sake of better performance now that I know where the overhead lies.
    
    With that, I think I am done. I did well in the end. I drilled down into this issue and with some more effort, I will resolve it.

---
+++ Spiral V5/V5 With The New Backend 2/add_interpreter.fsx
@@ -0,0 +1,37 @@
+﻿// I am testing superlinear performance behavior in the main partial evaluator.
+// It checks out. Most likely, the problems I am having are due to excessive stack memory usage.
+
+type Expr =
+    | Lit of int
+    | Add of Expr * Expr
+
+let rec intr = function
+    | Lit _ as x -> x
+    | Add(Lit a,Lit b) -> Lit <| a + b
+    | Add(a,b) -> intr <| Add(intr a, intr b)
+
+let rec intr_cps x ret =
+    match x with
+    | Lit _ as x -> ret x
+    | Add(Lit a,Lit b) -> Lit (a + b) |> ret
+    | Add(a,b) -> 
+        intr_cps a <| fun a ->
+            intr_cps b <| fun b ->
+                intr_cps (Add(a, b)) ret
+
+let rec add n =
+    if n > 0 then Add(Lit 1, add (n-1))
+    else Lit 1
+
+open System.Threading
+let run f = Thread(ThreadStart f,134217728*4).Start() // It stack overflows without being spun on a separate thread.
+    
+run <| fun _ ->
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+    let n = 1000*1000
+    printfn "%A" (intr (add n))
+    printfn "time elapsed = %A" stopwatch.Elapsed
+    
+    stopwatch.Restart()
+    printfn "%A" (intr_cps (add n) id)
+    printfn "time elapsed for cps = %A" stopwatch.Elapsed
--- Spiral V5/V5 With The New Backend 2/typed_expr_hashing_benchmark.fsx
+++ Spiral V5/V5 With The New Backend 2/typed_expr_hashing_benchmark.fsx
@@ -14,18 +14,18 @@ let v t = TyV(tag(),t)
 
 let vv' n () = vv 10 n
 let v' t () = v t
-let l = v' IntT |> vv' |> vv' |> vv' |> vv' |> vv' |> vv' |> fun x -> x()
+let l = v' IntT |> vv' |> vv' |> vv' |> vv' |> vv' |> vv' |> vv' |> fun x -> x()
 
 let rec free_vars = function
     | TyV (x,_) -> Set.singleton x
     | TyVV l -> Array.map free_vars l |> Set.unionMany
 
 let rec hash_typed_expr = function
-    | TyV (x,_) -> x
+    | TyV (x,_) -> 1
     | TyVV l -> Array.fold (fun s x -> s + hash_typed_expr x) 0 l
 
 #time
-for i = 1 to 10 do hash_typed_expr l |> ignore // 0.2s
-for i = 1 to 10 do l.GetHashCode() |> ignore // 0.5s
-for i = 1 to 10 do free_vars l |> ignore // 4.7s
+for i = 1 to 1 do hash_typed_expr l |> ignore // 0.2s
+for i = 1 to 1 do l.GetHashCode() |> ignore // 0.5s
+for i = 1 to 1 do free_vars l |> ignore // 4.7s
 
