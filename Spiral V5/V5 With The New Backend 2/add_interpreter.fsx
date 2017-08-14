5e3174ec42036a057c950a1ad4aaeb196fb5dba4
Marko Grdinić <mrakgr@gmail.com>
2017-08-14 09:07:44 +0200

    One idea that I had yesterday was to simply remove types from TyVV and TyEnv. That way I would not need to instantiate them every time, and I can make a custom type equality check that does not allocate.
    I think that would be better than what I have now. I think I went too wild on nodification.
    
    Before that though, what I want to do is not go to CPS which would squash the poor scaling issues, but make sure `apply` is called tail recursively. If I am right that should take care of the issue with the loop example. Let me try it.

--- Spiral V5/V5 With The New Backend 2/add_interpreter.fsx
+++ Spiral V5/V5 With The New Backend 2/add_interpreter.fsx
@@ -20,18 +20,39 @@ let rec intr_cps x ret =
                 intr_cps (Add(a, b)) ret
 
 let rec add n =
-    if n > 0 then Add(Lit 1, add (n-1))
+    if n > 1 then Add(Lit 1, add (n-1))
     else Lit 1
 
 open System.Threading
-let run f = Thread(ThreadStart f,134217728*4).Start() // It stack overflows without being spun on a separate thread.
+
+let mem = 1024*1024*512 // ~536mb
+// It stack overflows without being spun on a separate thread.
+// By default, the program only has a few mb of stack memory at its disposal.
+let run f = Thread(ThreadStart f,mem).Start() 
     
 run <| fun _ ->
-    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
-    let n = 1000*1000
-    printfn "%A" (intr (add n))
-    printfn "time elapsed = %A" stopwatch.Elapsed
+    let f n =
+        let x = add n
+        let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+        printfn "%A" (intr x)
+        printfn "n_%i_std = %A" n stopwatch.Elapsed
     
-    stopwatch.Restart()
-    printfn "%A" (intr_cps (add n) id)
-    printfn "time elapsed for cps = %A" stopwatch.Elapsed
+        stopwatch.Restart()
+        printfn "%A" (intr_cps x id)
+        printfn "n_%i_cps = %A" n stopwatch.Elapsed
+    f <| 1000*1000/2
+    f <| 1000*1000
+    f <| 1000*1000*2
+
+//Lit 500000
+//n_500000_std = 00:00:00.7764730
+//Lit 500000
+//n_500000_cps = 00:00:00.0800371
+//Lit 1000000
+//n_1000000_std = 00:00:02.9531043
+//Lit 1000000
+//n_1000000_cps = 00:00:00.1941828
+//Lit 2000000
+//n_2000000_std = 00:00:13.7823780
+//Lit 2000000
+//n_2000000_cps = 00:00:00.2767752
