cbc99992252598e24aaea014f08ffd5df8905401
Marko Grdinić <mrakgr@gmail.com>
2017-09-07 14:23:16 +0200

    I had enough of the book. It is just blathering on and on. Also I have no idea why the guy choose C++ as a language to represent these concepts. Am I being trolled here?
    Well, let me just continue.
    
    Today I feel like being a jellyfish. In part I am still recovering from yesterday, but I did quite a bit of useful work got get the && and || operators into shape.
    
    Actually, I just had another useful idea how to virtualize the && and || fully. I will add the AndOverloaded and OrOverloaded into the language and have the parser produce thme. Then those two will just check the environment...where the && and || functions won't be present because && and || will be macros. Unless I then hack the prepass to look for AndOverload and OrOverload.
    
    ...Actually doing this would be quite easy. Let me do it and then I will be done with this.

--- Spiral V5/V5 With The New Backend 2/SpiralV5Tests.fs
+++ Spiral V5/V5 With The New Backend 2/SpiralV5Tests.fs
@@ -636,73 +636,73 @@ inm _ = on_log z
 on_succ (x+y+z) // Tuple2(20L, Tuple1(2L, 7L, 11L))
     """
 
-let test55 =
-    "test55",[array;console;parsing4],"Does the v4 of the (monadic) parsing library work? Birthday Cake Candles problem.",
-    """
-//https://www.hackerrank.com/challenges/birthday-cake-candles
-open Console
-open Parsing
-
-inl solution x = 
-    inl f = 
-        inm n = parse_int
-        inm ar = parse_n_array parse_int n 
-        Array.foldl (inl (min,score as s) x ->
-            if x > score then (1,x)
-            elif x = score then (min+1,score)
-            else s
-            ) (dyn (0,mscorlib ."System.Int64" .MinValue)) ar
-        |> fst
-        |> writeline
-        |> succ
-    f x
-
-run_with_unit_ret (readall()) solution
-    """
-
-let test56 =
-    "test56",[array;console;parsing4],"Does the v4 of the (monadic) parsing library work? Diagonal Sum Difference problem.",
-    """
-//https://www.hackerrank.com/challenges/diagonal-difference
-open Console
-open Parsing
-
-inl abs x = if x >= 0 then x else -x
-
-inl f =
-    inm n = parse_int
-    inm ar = parse_n_array parse_int (n*n)
-    inl load row col = 
-        inl f x = x >= 0 || x < n
-        assert (f row && f col) "Out of bounds."
-        ar (n * row + col)
-    met rec loop (!dyn i) (d1,d2 as s) =
-        if i < n then loop (i+1) (d1 + load i i, d2 + load i (n-i-1))
-        else s
-        : s
-    inl a,b = loop 0 (0,0)
-    abs (a-b) 
-    |> writeline
-    |> succ
-
-run_with_unit_ret (readall()) f
-        """
-
-let test57 =
-    "test57",[array;console;parsing4],"Speed test for the v4 of the parsing library.",
-    """
-//https://www.hackerrank.com/challenges/diagonal-difference
-open Console
-open Parsing
-
-inl abs x = if x >= 0 then x else -x
-
-inl f = 
-    Tuple.repeat 240 parse_int |> tuple
-    |>> (Tuple.foldl (+) 0 >> writeline >> succ)
-    
-run_with_unit_ret (readall()) f
-        """
+//let test55 =
+//    "test55",[array;console;parsing4],"Does the v4 of the (monadic) parsing library work? Birthday Cake Candles problem.",
+//    """
+////https://www.hackerrank.com/challenges/birthday-cake-candles
+//open Console
+//open Parsing
+//
+//inl solution x = 
+//    inl f = 
+//        inm n = parse_int
+//        inm ar = parse_n_array parse_int n 
+//        Array.foldl (inl (min,score as s) x ->
+//            if x > score then (1,x)
+//            elif x = score then (min+1,score)
+//            else s
+//            ) (dyn (0,mscorlib ."System.Int64" .MinValue)) ar
+//        |> fst
+//        |> writeline
+//        |> succ
+//    f x
+//
+//run_with_unit_ret (readall()) solution
+//    """
+//
+//let test56 =
+//    "test56",[array;console;parsing4],"Does the v4 of the (monadic) parsing library work? Diagonal Sum Difference problem.",
+//    """
+////https://www.hackerrank.com/challenges/diagonal-difference
+//open Console
+//open Parsing
+//
+//inl abs x = if x >= 0 then x else -x
+//
+//inl f =
+//    inm n = parse_int
+//    inm ar = parse_n_array parse_int (n*n)
+//    inl load row col = 
+//        inl f x = x >= 0 || x < n
+//        assert (f row && f col) "Out of bounds."
+//        ar (n * row + col)
+//    met rec loop (!dyn i) (d1,d2 as s) =
+//        if i < n then loop (i+1) (d1 + load i i, d2 + load i (n-i-1))
+//        else s
+//        : s
+//    inl a,b = loop 0 (0,0)
+//    abs (a-b) 
+//    |> writeline
+//    |> succ
+//
+//run_with_unit_ret (readall()) f
+//        """
+//
+//let test57 =
+//    "test57",[array;console;parsing4],"Speed test for the v4 of the parsing library.",
+//    """
+////https://www.hackerrank.com/challenges/diagonal-difference
+//open Console
+//open Parsing
+//
+//inl abs x = if x >= 0 then x else -x
+//
+//inl f = 
+//    Tuple.repeat 240 parse_int |> tuple
+//    |>> (Tuple.foldl (+) 0 >> writeline >> succ)
+//    
+//run_with_unit_ret (readall()) f
+//        """
 
 let test58 =
     "test58",[array;console],"Does the fold function get duplicated?",
@@ -715,74 +715,80 @@ Array.foldl (inl a,b c,d -> a+c,b+d) (dyn (1,2)) ar
 |> writeline
     """
 
-let test59 =
-    "test59",[parsing4;console],"Does sprintf work for v4?",
+//let test59 =
+//    "test59",[parsing4;console],"Does sprintf work for v4?",
+//    """
+//inl a = dyn 1
+//inl b = dyn 2
+//Parsing.sprintf "%i + %i = %i" a b (a+b) |> ignore
+//Console.printfn "(%i,%f,%b,%s)" 1 2.0 true "4"
+//    """
+//
+//let test60 =
+//    "test60",[array;queue;parsing4;console],"https://www.hackerrank.com/challenges/saveprincess",
+//    """
+//// A simple dynamic programming problem. It wouldn't be hard to do in F#, but Spiral
+//// gives some novel challenges regarding it.
+//open Parsing
+//
+//inl Cell =
+//    type
+//        .Empty
+//        .Princess
+//        .Mario
+//
+//inl empty = pchar 'e' >>% Cell .Empty
+//inl princess = pchar 'p' >>% Cell .Princess
+//inl mario = pchar '-' >>% Cell .Mario
+//
+//inl cell = empty <|> princess <|> mario
+//
+//inl parse_cols n = parse_n_array n cell .>> spaces
+//inl parse_rows n = parse_n_array n (parse_cols n)
+//
+//inl solve n field =
+//    inl cur_pos on_succ =
+//        met rec loop1 (!dyn row) =
+//            met rec loop2 (!dyn col) =
+//                if col < n then
+//                    match field row col with
+//                    | .Mario -> on_succ row col
+//                    | _ -> loop2 (col+1)
+//                else loop1 (row+1)
+//            if row < n then loop2 0
+//            else failwith "Mario not found."
+//        loop1 0
+//    cur_pos <| inl row col ->
+//    // init the arrays
+//    inl cells_visited = Array.init n (inl _ -> Array.init n false)
+//    cells_visited row col <- true
+//
+//    inl ar = Array.singleton ((row,col), List.empty string)
+//
+//    met rec loop on_fail on_succ =
+//        inl row,col = Queue.dequeue p
+//        match ar row col with
+//        | .Princess -> on_succ()
+//        | _ ->
+//            inl up = (row-1,col),"UP"
+//            inl down = (row+1,col),"DOWN"
+//            inl left = (row,col-1),"LEFT"
+//            inl right = (row,col+1),"RIGHT"
+//            inl is_valid x = x >= 0 && x < n
+//            inl is_in_range (row,col) = is_valid row && is_valid col
+//            inl select (p,move) rest =
+//                if is_in_range p then 
+//
+//inl f =
+//    inm n = parse_int
+//    inm ar = parse_rows n
+//
+//    """
+
+let test61 =
+    "test61",[],"Does the handling of || and && operators work correctly?",
     """
-inl a = dyn 1
-inl b = dyn 2
-Parsing.sprintf "%i + %i = %i" a b (a+b) |> ignore
-Console.printfn "(%i,%f,%b,%s)" 1 2.0 true "4"
-    """
-
-let test60 =
-    "test60",[array;queue;parsing4;console],"https://www.hackerrank.com/challenges/saveprincess",
-    """
-// A simple dynamic programming problem. It wouldn't be hard to do in F#, but Spiral
-// gives some novel challenges regarding it.
-open Parsing
-
-inl Cell =
-    type
-        .Empty
-        .Princess
-        .Mario
-
-inl empty = pchar 'e' >>% Cell .Empty
-inl princess = pchar 'p' >>% Cell .Princess
-inl mario = pchar '-' >>% Cell .Mario
-
-inl cell = empty <|> princess <|> mario
-
-inl parse_cols n = parse_n_array n cell .>> spaces
-inl parse_rows n = parse_n_array n (parse_cols n)
-
-inl solve n field =
-    inl cur_pos on_succ =
-        met rec loop1 (!dyn row) =
-            met rec loop2 (!dyn col) =
-                if col < n then
-                    match field row col with
-                    | .Mario -> on_succ row col
-                    | _ -> loop2 (col+1)
-                else loop1 (row+1)
-            if row < n then loop2 0
-            else failwith "Mario not found."
-        loop1 0
-    cur_pos <| inl row col ->
-    // init the arrays
-    inl cells_visited = Array.init n (inl _ -> Array.init n false)
-    cells_visited row col <- true
-
-    inl ar = Array.singleton ((row,col), List.empty string)
-
-    met rec loop on_fail on_succ =
-        inl row,col = Queue.dequeue p
-        match ar row col with
-        | .Princess -> on_succ()
-        | _ ->
-            inl up = (row-1,col),"UP"
-            inl down = (row+1,col),"DOWN"
-            inl left = (row,col-1),"LEFT"
-            inl right = (row,col+1),"RIGHT"
-            inl is_valid x = x >= 0 && x < n
-            inl is_in_range (row,col) = is_valid row && is_valid col
-            inl select (p,move) rest =
-                if is_in_range p then 
-
-inl f =
-    inm n = parse_int
-    inm ar = parse_rows n
-
+false || true
     """
 
 let tests =
@@ -792,7 +798,7 @@ let tests =
     test20;test21;test22;test23;test24;test25;test26;test27;test28;test29
     test30;test31;test32;test33;test35;test38
     test40;test42;test43;test44;test45;test46;test47;test48;test49
-    test54;test55;test56;test57;test58;test59
+    test54;test58;test61
     hacker_rank_1
     |] |> Array.map module_
 
@@ -812,6 +818,6 @@ let run_test name is_big_test =
 
     System.Threading.Thread(System.Threading.ThreadStart f, 1024*1024*16).Start()
 
-run_test "test59" false
+run_test "test61" false
 
 
