0b5a821bce70a5e9a73e66ac5e046b756e95d9a3
Marko Grdinić <mrakgr@gmail.com>
2017-09-20 10:56:44 +0200

    Weak tables are no good as they do not use stuctural equality. This is a bust.
    Let me check out the Ocaml and Haskell libraries for hash consing.
    
    I am at a loss here. Are strong references the only choice?

---
+++ Spiral V5/V5 With The New Backend 2/weak_conditional_table.fsx
@@ -0,0 +1,24 @@
+﻿// The weak tables are no good for hash consing as they don't use structural equality.
+
+open System
+open System.Runtime.CompilerServices
+
+type ManagedClass = Var of int
+
+let mc1 = Var 1
+let mc2 = Var 2
+let mc3 = Var 3
+
+let f () =
+    let cwt = ConditionalWeakTable()
+    cwt.Add(mc1, ref mc1)
+    cwt.Add(mc2, ref mc2)
+    cwt.Add(mc3, ref mc3)
+
+    cwt
+
+let cwt = f ()
+//GC.Collect()
+match cwt.TryGetValue (Var 1) with
+| true, v -> printfn "%A" v
+| false, _ -> printfn "no key found"
