efef23d9aff789e9d679f40e8bf46a947ce6932f
Marko Grdinić <mrakgr@gmail.com>
2017-10-09 19:33:05 +0200

    I had nothing better to do so I decided to fill out the skeleton.
    Well, here is the thing. What a waste of time. This is OO .NET style. What a pile of crap.
    
    I'll give it a try tomorrow. Hopefully passing a type like this as an argument won't smash the thing...
    
    Actually, let me give it a try.

--- Spiral V5/V5 With The New Backend 2/system_type_inheritance.fsx
+++ Spiral V5/V5 With The New Backend 2/system_type_inheritance.fsx
@@ -1,29 +1,41 @@
 ﻿open System
-open System.Reflection
 
-let o = typeof<Object>
-o.GUID
+type SpiralType() =
+    inherit Type()
 
-//type SpiralType() =
-//    inherit Type()
-//
-//    override __.GUID with get() = o.GUID
-//
-//    override __.InvokeMember(name:string,invokeAttr: Reflection.BindingFlags,binder: Reflection.Binder,target: obj,args: obj[],
-//                             modifiers: Reflection.ParameterModifier[],culture: Globalization.CultureInfo,namedParameters: string[]) = null
-//
-//    override __.Module with get() = o.Module
-//    override __.Assembly with get() = o.Assembly
-//    override __.FullName with get() = "SpiralType"
-//    override __.Namespace with get() = "Spiral"
-//    override __.AssemblyQualifiedName with get() = o.AssemblyQualifiedName
-//    override __.BaseType with get() = o.BaseType
-//
-//    override __.GetConstructorImpl(bindingAttr: Reflection.BindingFlags, binder: Reflection.Binder, callConvention: Reflection.CallingConventions,
-//                                   types: Type[], modifiers: Reflection.ParameterModifier[]) = null
-//
-//    override __.GetConstructors(bindingAttr: Reflection.BindingFlags) = null
-//
-//    override __.GetMethodImpl(name:string, bindingAttr:Reflection.BindingFlags, binder:Reflection.Binder, 
-//                              callConvention:Reflection.CallingConventions, types:Type[], modifiers:Reflection.ParameterModifier[]) = null
-
+    override __.IsDefined(_,_) = false
+    override __.GetCustomAttributes(_,_) = null
+    override __.GetCustomAttributes(_) = null
+    override __.GUID with get() = failwith "Don't call this."
+    override __.InvokeMember(_,_,_,_,_,_,_,_) = null
+    override __.Module with get() = failwith "Don't call this.": Reflection.Module
+    override __.Assembly with get() = null
+    override __.Name with get() = null
+    override __.FullName with get() = null
+    override __.Namespace with get() = null
+    override __.AssemblyQualifiedName with get() = null
+    override __.BaseType with get() = null
+    override __.GetConstructorImpl(_,_,_,_,_) = null
+    override __.GetConstructors(_) = null
+    override __.GetMethodImpl(_,_,_,_,_,_) = null
+    override __.GetMethods(_) = null
+    override __.GetField(_,_) = null
+    override __.GetFields(_) = null
+    override __.GetInterface(_,_) = null
+    override __.GetInterfaces() = null
+    override __.GetEvent(_,_) = null
+    override __.GetEvents(_) = null
+    override __.GetPropertyImpl(_,_,_,_,_,_) = null
+    override __.GetProperties(_) = null
+    override __.GetNestedType(_,_) = null
+    override __.GetNestedTypes(_) = null
+    override __.GetMembers(_) = null
+    override __.GetAttributeFlagsImpl() = failwith "Don't call this."
+    override __.IsArrayImpl() = false
+    override __.IsByRefImpl() = false
+    override __.IsPointerImpl() = false
+    override __.IsPrimitiveImpl() = false
+    override __.IsCOMObjectImpl() = false
+    override __.GetElementType() = null
+    override __.HasElementTypeImpl() = false
+    override __.UnderlyingSystemType with get() = null
