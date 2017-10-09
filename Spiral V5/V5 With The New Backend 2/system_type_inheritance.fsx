b266753c405265668e5fc663fcc26baf1569eda2
Marko Grdinić <mrakgr@gmail.com>
2017-10-09 20:15:35 +0200

    I did a bit more, but it was fruitless.
    https://stackoverflow.com/questions/46652587/how-to-make-a-minimal-useful-implementation-of-a-class-that-inherits-from-system
    
    Thankfully right now I have neough to make this question. Hopefully, I will get something this time.
    
    I had enough of this for the day.

--- Spiral V5/V5 With The New Backend 2/system_type_inheritance.fsx
+++ Spiral V5/V5 With The New Backend 2/system_type_inheritance.fsx
@@ -1,41 +1,61 @@
 ﻿open System
 
-type SpiralType() =
+type SpiralDU =
+    | IntT
+    | StringT
+    | TupleT of SpiralDU list
+
+type SpiralTypeToken(metadata: SpiralDU) =
     inherit Type()
 
-    override __.IsDefined(_,_) = false
-    override __.GetCustomAttributes(_,_) = null
-    override __.GetCustomAttributes(_) = null
-    override __.GUID with get() = failwith "Don't call this."
-    override __.InvokeMember(_,_,_,_,_,_,_,_) = null
-    override __.Module with get() = failwith "Don't call this.": Reflection.Module
-    override __.Assembly with get() = null
-    override __.Name with get() = null
-    override __.FullName with get() = null
-    override __.Namespace with get() = null
-    override __.AssemblyQualifiedName with get() = null
-    override __.BaseType with get() = null
-    override __.GetConstructorImpl(_,_,_,_,_) = null
-    override __.GetConstructors(_) = null
-    override __.GetMethodImpl(_,_,_,_,_,_) = null
-    override __.GetMethods(_) = null
-    override __.GetField(_,_) = null
-    override __.GetFields(_) = null
-    override __.GetInterface(_,_) = null
-    override __.GetInterfaces() = null
-    override __.GetEvent(_,_) = null
-    override __.GetEvents(_) = null
-    override __.GetPropertyImpl(_,_,_,_,_,_) = null
-    override __.GetProperties(_) = null
-    override __.GetNestedType(_,_) = null
-    override __.GetNestedTypes(_) = null
-    override __.GetMembers(_) = null
-    override __.GetAttributeFlagsImpl() = failwith "Don't call this."
-    override __.IsArrayImpl() = false
-    override __.IsByRefImpl() = false
-    override __.IsPointerImpl() = false
-    override __.IsPrimitiveImpl() = false
-    override __.IsCOMObjectImpl() = false
-    override __.GetElementType() = null
-    override __.HasElementTypeImpl() = false
-    override __.UnderlyingSystemType with get() = null
+    member val Metadata = metadata
+
+    override __.IsDefined(a,b) = base.IsDefined(a,b)
+    override __.GetCustomAttributes(a,b) = base.GetCustomAttributes(a,b)
+    override __.GetCustomAttributes(a) = base.GetCustomAttributes(a)
+    override __.GUID with get() = base.GUID
+    override __.InvokeMember(a,b,c,d,e,f,g,h) = base.InvokeMember(a,b,c,d,e,f,g,h)
+    override __.Module with get() = base.Module
+    override __.Assembly with get() = base.Assembly
+    override __.Name with get() = base.Name
+    override __.FullName with get() = base.FullName
+    override __.Namespace with get() = base.Namespace
+    override __.AssemblyQualifiedName with get() = base.AssemblyQualifiedName
+    override __.BaseType with get() = base.BaseType
+    override __.GetConstructorImpl(a,b,c,d,e) = base.GetConstructorImpl(a,b,c,d,e)
+    override __.GetConstructors(a) = base.GetConstructors(a)
+    override __.GetMethodImpl(a,b,c,d,e,f) = base.GetMethodImpl(a,b,c,d,e,f)
+    override __.GetMethods(a) = base.GetMethods(a)
+    override __.GetField(a,b) = base.GetField(a,b)
+    override __.GetFields(a) = base.GetFields(a)
+    override __.GetInterface(a,b) = base.GetInterface(a,b)
+    override __.GetInterfaces() = base.GetInterfaces()
+    override __.GetEvent(a,b) = base.GetEvent(a,b)
+    override __.GetEvents(a) = base.GetEvents(a)
+    override __.GetPropertyImpl(a,b,c,d,e,f) = base.GetPropertyImpl(a,b,c,d,e,f)
+    override __.GetProperties(a) = base.GetProperties(a)
+    override __.GetNestedType(a,b) = base.GetNestedType(a,b)
+    override __.GetNestedTypes(a) = base.GetNestedTypes(a)
+    override __.GetMembers(a) = base.GetMembers(a)
+    override __.GetAttributeFlagsImpl() = base.GetAttributeFlagsImpl()
+    override __.IsArrayImpl() = base.IsArrayImpl()
+    override __.IsByRefImpl() = base.IsByRefImpl()
+    override __.IsPointerImpl() = base.IsPointerImpl()
+    override __.IsPrimitiveImpl() = base.IsPrimitiveImpl()
+    override __.IsCOMObjectImpl() = base.IsCOMObjectImpl()
+    override __.GetElementType() = base.GetElementType()
+    override __.HasElementTypeImpl() = base.HasElementTypeImpl()
+    override __.UnderlyingSystemType with get() = base.UnderlyingSystemType
+
+open System.Collections.Generic
+open System.Reflection
+let x = Reflection.Assembly.Load("mscorlib")
+let dic' = x.GetType("System.Collections.Generic.Dictionary`2")
+
+let key = TupleT [IntT; IntT]
+let st = SpiralTypeToken(key) :> Type
+let dic_ins_typ = dic'.MakeGenericType [|typeof<int>;typeof<int>|]
+let dic_ins_typ' = dic'.MakeGenericType [|st;typeof<int>|]
+
+dic_ins_typ.GetMethod("get_Item",[|typeof<int>|]) // Works
+dic_ins_typ'.GetMethod("get_Item",[|st|]) // System.NotSupportedException: Specified method is not supported.
