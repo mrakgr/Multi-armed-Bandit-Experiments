42f48edf0ed151a0fd6064b796e76362f50debfa
Marko Grdinić <mrakgr@gmail.com>
2017-10-09 18:18:02 +0200

    It seems that to implement type I will have ot make like fucking 30 dummy methods fist. Fuck .NET seriously.
    I do not feel like doing this today. I think I will just do this tomorrow and then plug it in. After that I will have full .NET integration. No problem, no problem at all.
    
    It is time for me to have some fun.
    
    After I am done with this, I will resume doing HackerRank problems.

---
+++ Spiral V5/V5 With The New Backend 2/system_type_inheritance.fsx
@@ -0,0 +1,29 @@
+﻿open System
+open System.Reflection
+
+let o = typeof<Object>
+o.GUID
+
+//type SpiralType() =
+//    inherit Type()
+//
+//    override __.GUID with get() = o.GUID
+//
+//    override __.InvokeMember(name:string,invokeAttr: Reflection.BindingFlags,binder: Reflection.Binder,target: obj,args: obj[],
+//                             modifiers: Reflection.ParameterModifier[],culture: Globalization.CultureInfo,namedParameters: string[]) = null
+//
+//    override __.Module with get() = o.Module
+//    override __.Assembly with get() = o.Assembly
+//    override __.FullName with get() = "SpiralType"
+//    override __.Namespace with get() = "Spiral"
+//    override __.AssemblyQualifiedName with get() = o.AssemblyQualifiedName
+//    override __.BaseType with get() = o.BaseType
+//
+//    override __.GetConstructorImpl(bindingAttr: Reflection.BindingFlags, binder: Reflection.Binder, callConvention: Reflection.CallingConventions,
+//                                   types: Type[], modifiers: Reflection.ParameterModifier[]) = null
+//
+//    override __.GetConstructors(bindingAttr: Reflection.BindingFlags) = null
+//
+//    override __.GetMethodImpl(name:string, bindingAttr:Reflection.BindingFlags, binder:Reflection.Binder, 
+//                              callConvention:Reflection.CallingConventions, types:Type[], modifiers:Reflection.ParameterModifier[]) = null
+
