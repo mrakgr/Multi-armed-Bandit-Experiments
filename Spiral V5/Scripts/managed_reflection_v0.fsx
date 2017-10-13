77e99ffcf2645fc7a823130b61060a1183a893b2
Marko Grdinić <mrakgr@gmail.com>
2017-10-13 16:38:08 +0200

    Let me revert to get rid of all the packages.

---
+++ Spiral V5/Scripts/managed_reflection_v0.fsx
@@ -0,0 +1,11 @@
+﻿//#load "load-references-release.fsx"
+//
+//open System.Runtime.CompilerServices
+//
+//type SpiralType =
+//    | IntT
+//    | StringT
+//    | TupleT of SpiralType list
+//
+//open Managed.Reflection
+//
