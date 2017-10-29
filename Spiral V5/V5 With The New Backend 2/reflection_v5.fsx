0e0d83f5be9d6b8b3d21b3d6505a3d4f5d476aab
Marko Grdinić <mrakgr@gmail.com>
2017-10-29 17:57:10 +0100

    5:20pm. I am thinking. Fields I can do as constants.
    I can see all of the steps towards doing the extern module. I think I could manage it in 2-3 days. I might want to do CudaDeviceVariable and Dictionary on my own first.
    
    But besides that, how do I separate out static methods from instance methods via reflection? I still actually do not know how to do that.
    
    5:25pm. Let me investiage this as the last thing I do for the day. Tomorrow then I will be able to do the prototype extern modules.
    ...
         IsSpecialName = false;
         IsStatic = true;
         IsVirtual = false;
    There is the IsStatic attribute clearly enough. I thought it was not there.
    
    Huh? Let me try the dictionary.
    
    Well, there is the IsStatic property. I wonder how I missed it last time? Let me try loading the assembly.
    
    5:35pm.
    open System
    let x = Reflection.Assembly.Load("mscorlib")
    let dic' = x.GetType("System.Collections.Generic.Dictionary`2")
    let dic_met = dic'.GetMethods()
    dic_met.[0].IsStatic
    
    let str = x.GetType("System.String")
    let str_met = str.GetMethods()
    str_met.[0].IsStatic
    
    let int = x.GetType("System.Int32")
    let int_fld = int.GetFields()
    int_fld.[0].IsStatic
    
    So the fields have static methods too just as I expected.
    
    I am thinking things through. It seems that in fact the Extern scheme that I have in mind should be enough to cover the richness of the full .NET ecosystem. The types will in fact be interchangeable and I will be able to get rid of those dumb conversions.
    
    I never intended for `type` to be used like this, I haven't even imagined that something like this could be possible, but it will work.
    
    I think I will have to remove the restriction that only lists and unions can be recursive types, I still have that stupid thing in the codegen. I will add maps to that, but I also need to add variables since now I have the LayoutT. Things have changed since I had that restriction instated.
    
    I think with this I will call it day. No way do I feel like starting something else at 5:50pm.

---
+++ Spiral V5/V5 With The New Backend 2/reflection_v5.fsx
@@ -0,0 +1,13 @@
+﻿open System
+let x = Reflection.Assembly.Load("mscorlib")
+let dic' = x.GetType("System.Collections.Generic.Dictionary`2")
+let dic_met = dic'.GetMethods()
+dic_met.[0].IsStatic
+
+let str = x.GetType("System.String")
+let str_met = str.GetMethods()
+str_met.[0].IsStatic
+
+let int = x.GetType("System.Int32")
+let int_fld = int.GetFields()
+int_fld.[0].IsStatic
