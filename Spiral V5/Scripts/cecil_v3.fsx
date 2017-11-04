ca454e1f65452474fb4a0f6dffcc6a49a70d67d7
Marko Grdinić <mrakgr@gmail.com>
2017-11-04 17:51:43 +0100

    Here is the experimentation that I did. With this I have enough to establish equality between type definitions. With this I can use them in dictionaries and hence have enough to actually do the rewrite.
    One thing I'd like to know is how the assemblies are loaded from the global namespace, I think it was called MAC or something. Well, figuring that out won't be too hard, it will be an easy question for SO.
    
    The important thing is that the rewrite now that I have all the pieces won't be hard to do at all.
    
    I'll do it all tomorrow. Today, I am too tired.
    
    It is time to call it a day.

---
+++ Spiral V5/Scripts/cecil_v3.fsx
@@ -0,0 +1,37 @@
+﻿#load "load-references-release.fsx"
+
+open System
+open Mono.Cecil
+open Mono.Cecil.Rocks
+
+let mscorlib_path = @"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\mscorlib.dll"
+let mscorlib = AssemblyDefinition.ReadAssembly(mscorlib_path)
+
+let dictionary_type =
+    mscorlib.Modules.[0].Types
+    |> Seq.find (fun x -> x.Name = "Dictionary`2")
+
+let add_method =
+    dictionary_type.Methods
+    |> Seq.find (fun x -> x.Name = "Add")
+
+let mets =
+    mscorlib.MainModule.Types
+    |> Seq.collect (fun x ->
+        x.Methods
+        )
+    |> Seq.filter (fun x ->
+        x.Parameters
+        |> Seq.exists (fun x -> x.ParameterType.ContainsGenericParameter)
+        )
+    |> Seq.toArray
+
+let met = mets |> Array.find (fun x -> x.Name.Contains "AsReadOnly")
+
+let readonly_col = met.ReturnType.Resolve()
+let readonly_col' = 
+    mscorlib.MainModule.Types
+    |> Seq.find (fun x -> x.Name.Contains "ReadOnlyCollection`1")
+
+readonly_col = readonly_col' // true
+Object.ReferenceEquals(readonly_col,readonly_col') // true
