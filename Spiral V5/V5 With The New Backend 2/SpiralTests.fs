8f5833ec7f7826021c8e8b738070eebf53630974
Marko Grdinić <mrakgr@gmail.com>
2017-11-04 08:58:51 +0100

    I've figured out why it got 2x slower. It is because I was running it in Debug mode. I switched to it to track down an exception and forgot to turn it back.
    Doh.
    
    Well, let me fix the interop now.

--- Spiral V5/V5 With The New Backend 2/SpiralTests.fs
+++ Spiral V5/V5 With The New Backend 2/SpiralTests.fs
@@ -438,7 +438,7 @@ let test33 = // 0.42s
     "test33",[],"Does a simple loop have superlinear scaling?",
     """
 inl rec loop = function
-    | i when !GT(i,0) -> loop (!Sub(i,1))
+    | i when i > 0 -> loop (i-1)
     | 0 -> ()
 loop 50000
     """
@@ -1908,7 +1908,7 @@ inl p =
 run_with_unit_ret (readall()) p
     """
 
-//rewrite_test_cache None //(Some(0,20))
+rewrite_test_cache None //(Some(0,20))
 
 output_test_to_temp test33
 |> printfn "%s"
